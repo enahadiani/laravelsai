a734ec385cfb9e8257ba89ab9c1ae9c73b73cd79
enahadiani <=>
2020-05-06 12:54:12 +0700

    fix notif

--- app/Http/Controllers/Apv/VerifikasiController.php
+++ app/Http/Controllers/Apv/VerifikasiController.php
@@ -24,6 +24,58 @@ class VerifikasiController extends Controller
         }
     }
 
+    
+    function sendNotif($title,$content,$token_player){ 	
+
+        try {
+
+            // $title = "Test SAI";
+            // $content = "Notif send from laravelsai";
+            // $token_player = array("6681074c-a789-46f5-a278-e1052d592ed1");
+            // $title = $title;
+            
+            $fields = array(
+                'app_id' => "5f0781d5-8856-4f3e-a2c7-0f95695def7e", //appid laravelsai
+                'include_player_ids' => $token_player,
+                'url' => "https://onesignal.com",
+                'data' => array(
+                    "foo" => "bar"
+                ),
+                'contents' => array(
+                    'en' => $content
+                ),
+                'headings' => array(
+                    'en' => $title
+                )
+            );
+            
+            $url = "https://onesignal.com/api/v1/notifications";
+            $client = new Client();
+            $response = $client->request('POST', $url, [
+                'body' => json_encode($fields),
+                'headers' => [
+                    'Content-Type'     => 'application/json; charset=utf-8',
+                    'Authorization' => 'Basic ZmY5ODczYTMtNTgwZS00YmQ4LWFmNTMtMzQxZDY4ODc3MWFh',
+                ]
+            ]);
+
+            if ($response->getStatusCode() == 200) { // 200 OK
+                $response_data = $response->getBody()->getContents();
+                
+                $data = json_decode($response_data,true);
+            }
+            $result = array('result' => $data, 'status'=>true, 'fields'=>$fields, 'message'=>'Send notif success!');
+            return $result;
+
+        } catch (BadResponseException $ex) {
+            $response = $ex->getResponse();
+            $res = json_decode($response->getBody(),true);
+            $result = array('message' => $res, 'status'=>false, 'fields'=> $fields);
+            return $result;
+        }
+
+    }
+
     /**
      * Show the form for creating a new resource.
      *
