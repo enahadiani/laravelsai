edfd0d6f0bf8f68e0f5e2c793b01c8eb802f28d3
oktapascal <oktaiscool@gmail.com>
2020-06-09 13:24:14 +0700

    Ganti form jadwal harian

--- app/Http/Controllers/Tarbak/JadwalHarianController.php
+++ app/Http/Controllers/Tarbak/JadwalHarianController.php
@@ -126,35 +126,6 @@
 
         }
 
-        public function getKalAkad($kode_sem,$kode_ta1,$kode_ta2,$kode_pp) {
-            try{
-                $client = new Client();
-                $kode_ta = $kode_ta1."/".$kode_ta2;
-                $response = $client->request('GET', $this->link.'kalender_akad?kode_sem='.$kode_sem
-                ."&kode_ta=".$kode_ta."&kode_pp=".$kode_pp,
-                [
-                    'headers' => [
-                        'Authorization' => 'Bearer '.Session::get('token'),
-                        'Accept'     => 'application/json',
-                    ]
-                ]);
-        
-                if ($response->getStatusCode() == 200) { // 200 OK
-                    $response_data = $response->getBody()->getContents();
-                    
-                    $data = json_decode($response_data,true);
-                    $data = $data["success"];
-                }
-                return response()->json(['data' => $data], 200); 
-            } catch (BadResponseException $ex) {
-                $response = $ex->getResponse();
-                $res = json_decode($response->getBody(),true);
-                $data['message'] = $res['message'];
-                $data['status'] = false;
-                return response()->json(['data' => $data], 200);
-            }
-        }
-
         public function delete($kode_pp,$kode_ta1,$kode_ta2,$kode_kelas,$nik,$kode_matpel) {
             try{
                 $client = new Client();
@@ -178,7 +149,7 @@
             } catch (BadResponseException $ex) {
                 $response = $ex->getResponse();
                 $res = json_decode($response->getBody(),true);
-                $data['message'] = $res;
+                $data['message'] = $res['message'];
                 $data['status'] = false;
                 return response()->json(['data' => $data], 200);
             }
@@ -186,42 +157,58 @@
 
         public function update(Request $request, $nik)
         {
-        $this->validate($request, [
+            $this->validate($request, [
                 'kode_pp' => 'required',
                 'kode_ta' => 'required',
-                'kode_sem' => 'required',
-                'tanggal' => 'required|array',
-                'agenda'=>'required|array'
+                'kode_matpel' => 'required',
+                'nik_guru' => 'required',
+                'kode_kelas' => 'required',
+                'kode_slot' => 'required|array',
+                'senin'=>'required|array',
+                'selasa' => 'required|array',
+                'rabu'=>'required|array',
+                'kamis' => 'required|array',
+                'jumat'=>'required|array',
+                'sabtu' => 'required|array',
+                'minggu'=>'required|array'
             ]);
 
-            try{
+            try{                
                 $fields = array (
                     'kode_pp' => $request->kode_pp,
                     'kode_ta' => $request->kode_ta,
-                    'kode_sem' => $request->kode_sem,
-                    'tanggal' => $request->tanggal,
-                    'agenda' => $request->agenda
+                    'kode_matpel' => $request->kode_matpel,
+                    'nik_guru' => $request->nik_guru,
+                    'kode_kelas' => $request->kode_kelas,
+                    'kode_slot'=>$request->kode_slot,
+                    'senin'=>$request->senin,
+                    'selasa'=>$request->selasa,
+                    'rabu'=>$request->rabu,
+                    'kamis'=>$request->kamis,
+                    'jumat'=>$request->jumat,
+                    'sabtu'=>$request->sabtu,
+                    'minggu'=>$request->minggu,
                   );
-        
+    
                 $client = new Client();
-                $response = $client->request('PUT', $this->link.'kalender_akad',[
+                $response = $client->request('PUT', $this->link.'jadwal_harian',[
                     'headers' => [
                         'Authorization' => 'Bearer '.Session::get('token'),
                         'Content-Type'     => 'application/json'
                     ],
                     'body' => json_encode($fields)
                 ]);
-                
                 if ($response->getStatusCode() == 200) { // 200 OK
                     $response_data = $response->getBody()->getContents();
                     
                     $data = json_decode($response_data,true);
-                    return response()->json(['data' => $data["success"],'fields'=>$fields], 200);  
+                    return response()->json(['data' => $data["success"]], 200);  
                 }
             } catch (BadResponseException $ex) {
                 $response = $ex->getResponse();
                 $res = json_decode($response->getBody(),true);
                 $data['message'] = $res['message'];
+                $data['fields'] = $fields;
                 $data['status'] = false;
                 return response()->json(['data' => $data], 200);
             }
