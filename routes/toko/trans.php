183ed5efd7f62f67139a2ef3c564f25a4759885e
oktapascal <oktaiscool@gmail.com>
2020-06-18 15:04:05 +0700

    Form Open Kasir TOKO

--- routes/toko/trans.php
+++ routes/toko/trans.php
@@ -7,3 +7,8 @@ use Illuminate\Support\Facades\Session;
 Route::get('penjualan-open', 'Toko\PenjualanController@getNoOpen');
 Route::get('penjualan-bonus/{kd_barang}/{tanggal}/{jumlah}/{harga}', 'Toko\PenjualanController@cekBonus');
 Route::post('penjualan', 'Toko\PenjualanController@store');
+
+//Open Kasir //
+Route::get('open-kasir', 'Toko\OpenKasirController@index');
+Route::post('open-kasir', 'Toko\OpenKasirController@store');
+Route::put('open-kasir/{nik}/{no_open}', 'Toko\OpenKasirController@update');
