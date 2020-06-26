c255fd58508997d8641464e50fdf298eea166dcc
enahadiani <=>
2020-06-26 15:36:23 +0700

    update laporan dago

--- resources/views/dago/rptFormReg.blade.php
+++ resources/views/dago/rptFormReg.blade.php
@@ -71,6 +71,12 @@
                                 <div class="kop" style="height:80px;width:100%">
                                     <img src="{{ asset('asset_elite/images/dago_form_logo.jpeg') }}" style="height:80px;width:100%">
                                 </div>
+                                <div style="height:20px;width:100%;font-size: 14px;background:#009ed72b" class="subkop text-left">
+                                    <span style="padding-right:10px">Website : www.dagowisata.com</span>
+                                    <span style="padding-right:10px">Email : info@dagowisata.com</span>
+                                    <span style="padding-right:10px">Twiter : @dagowisata</span>        
+                                    <span style="padding-right:10px">FB : facebook.com/dagowisata</span>
+                                </div>
                                 <table width='100%' class='table no-border' cellspacing='1' cellpadding='2'>
                                 <style>
                                     td,th{
