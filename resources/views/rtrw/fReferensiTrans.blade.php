3fd62a3e48312f05d0c734c429b06c6351460c7a
oktapascal <oktaiscool@gmail.com>
2020-07-01 10:36:38 +0700

    Fix form refrensi trans

--- resources/views/rtrw/fReferensiTrans.blade.php
+++ resources/views/rtrw/fReferensiTrans.blade.php
@@ -419,21 +419,9 @@
         });
 
         getPPSelect();
-        
-        $.fn.dataTable.ext.search.push(
-            function(setting,data,dataIndex) {
-                var ppSelected = $('#select-pp').val();
-                var pp = data[5].substr(0,2);
-
-                if(ppSelected === pp) {
-                    return true;
-                }
-                return false;
-            }
-        );
 
         $('#select-pp').change(function(){
-            dataTable.draw();
+          dataTable.search(this.value).draw();
         })
 
         $('#saku-datatable').on('click', '#btn-tambah', function(){
