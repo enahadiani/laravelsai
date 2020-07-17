17c0effdc6ef4c00790b3f3648143e093923553a
enahadiani <=>
2020-07-17 14:47:28 +0700

    update dago history pbyr

--- resources/views/dago/fHistoryPbyr.blade.php
+++ resources/views/dago/fHistoryPbyr.blade.php
@@ -331,6 +331,24 @@
             </div>
         </div>
     </div>
+    
+    <!-- MODAL SEARCH AKUN-->
+    <div class="modal" tabindex="-1" role="dialog" id="modal-search">
+        <div class="modal-dialog" role="document" style="max-width:600px">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title"></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END MODAL --> 
 </div>
 <script>
     function format_number(x){
