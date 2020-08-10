50e7e16fd532d843495a348b286e8e7663203c5c
oktapascal <oktaiscool@gmail.com>
2020-08-10 16:26:19 +0700

    Tagihan maintain SAI

--- app/Http/Controllers/Sai/TagihanMTController.php
+++ app/Http/Controllers/Sai/TagihanMTController.php
@@ -220,6 +220,7 @@ class TagihanMTController extends Controller
             'total_nilai' => 'required',
             'total_nilai_ppn' => 'required',
             'jenis'=> 'required',
+            'periode'=> 'required',
             'cust'=> 'required|array',
             'no_dokumen'=> 'required|array',
             'due_date'=> 'required|array',
@@ -228,8 +229,7 @@ class TagihanMTController extends Controller
             'nilai'=> 'required|array',
             'nilai_ppn'=> 'required|array',
         ]);
-
-        if($request->generate == TRUE) {
+    
             $explode_tgl = explode('/', $request->tanggal);
             $tgl = $explode_tgl[0];
             $bln = $explode_tgl[1];
@@ -250,6 +250,10 @@ class TagihanMTController extends Controller
                         'contents' => $request->keterangan,
                     ],
                     [
+                        'name' => 'periode',
+                        'contents' => $request->periode,
+                    ],
+                    [
                         'name' => 'total_nilai',
                         'contents' => intval(str_replace('.','', $request->total_nilai)),
                     ],
@@ -262,93 +266,34 @@ class TagihanMTController extends Controller
                         'contents' => $request->jenis,
                     ],
                 ];
-        
-            $fields_item = array();
-            if(count($request->item) > 0){
-                for($i=0;$i<count($request->item);$i++){
-                        $fields_item[$i] = array(
-                            'name'     => 'item[]',
-                            'contents' => $request->item[$i],
-                    );
-                }
-                $send_data = array_merge($fields,$fields_item);
-            }
 
-            $fields_nilai = array();
-            if(count($request->nilai) > 0){
-                for($i=0;$i<count($request->nilai);$i++){
-                        $fields_nilai[$i] = array(
-                            'name'     => 'nilai[]',
-                            'contents' => intval(str_replace('.','', $request->nilai[$i])),
-                    );
-                }
-                $send_data = array_merge($send_data,$fields_nilai);
-            }
+        $send_data = array_merge($fields);
+        // if(count($request->generate) > 0){
+        //     for($i=0;$i<count($request->generate);$i++){
+        //         $explode_due = explode('/', $request->due_date[$i]);
+        //         $tgl_due = $explode_due[0];
+        //         $bln_due = $explode_due[1];
+        //         $tahun_due = $explode_due[2];
+        //         $tanggal_due = $tahun_due."-".$bln_due."-".$tgl_due;
 
-            $fields_nilai_ppn = array();
-            if(count($request->nilai_ppn) > 0){
-                for($i=0;$i<count($request->nilai_ppn);$i++){
-                        $fields_nilai_ppn[$i] = array(
-                            'name'     => 'nilai_ppn[]',
-                            'contents' => intval(str_replace('.','', $request->nilai_ppn[$i])),
-                    );
-                }
-                $send_data = array_merge($send_data,$fields_nilai_ppn);
-            }
-
-            $fields_cust = array();
-            if(count($request->cust) > 0){
-                for($i=0;$i<count($request->cust);$i++){
-                        $cust[$i] = substr($request->cust[$i],0,5);
-                        $fields_cust[$i] = array(
-                            'name'     => 'kode_cust[]',
-                            'contents' => $cust[$i],
-                    );
-                }
-                $send_data = array_merge($send_data,$fields_cust);
-            }
-
-            $fields_kontrak = array();
-            if(count($request->no_kontrak) > 0){
-                for($i=0;$i<count($request->no_kontrak);$i++){
-                        $fields_kontrak[$i] = array(
-                            'name'     => 'no_kontrak[]',
-                            'contents' => $request->no_kontrak[$i],
-                    );
-                }
-                $send_data = array_merge($send_data,$fields_kontrak);
-            }
-
-            $fields_dokumen = array();
-            if(count($request->no_dokumen) > 0){
-                for($i=0;$i<count($request->no_dokumen);$i++){
-                        $fields_dokumen[$i] = array(
-                            'name'     => 'no_dokumen[]',
-                            'contents' => $request->no_dokumen[$i],
-                    );
-                }
-                $send_data = array_merge($send_data,$fields_dokumen);
-            }
-
-            $fields_date = array();
-            if(count($request->due_date) > 0){
-                for($i=0;$i<count($request->due_date);$i++){
-                        $explode_due = explode('/', $request->due_date[$i]);
-                        $tgl_due = $explode_due[0];
-                        $bln_due = $explode_due[1];
-                        $tahun_due = $explode_due[2];
-                        $tanggal_due = $tahun_due."-".$bln_due."-".$tgl_due;
-                        $fields_date[$i] = array(
-                            'name'     => 'due_date[]',
-                            'contents' => $tanggal_due,
-                    );
-                }
-                $send_data = array_merge($send_data,$fields_date);
-            }
-        }else{
-            return;
-        }
-
+        //         if($request->generate[$i] == 'true'){
+        //             $status = '1';
+        //         }else {
+        //             $status = '0';
+        //         }
+                    
+        //         $field_generate[$i] = array('name'=>'status[]','contents'=>$status);
+        //         $field_dokumen[$i] = array('name'=>'no_dokumen[]','contents'=>$request->no_dokumen[$i]);
+        //         $field_cust[$i] = array('name'=>'kode_cust[]','contents'=>substr($request->cust[$i],0,5));
+        //         $field_kontrak[$i] = array('name'=>'no_kontrak[]','contents'=>$request->no_kontrak[$i]);
+        //         $field_item[$i] = array('name'=>'item[]','contents'=>$request->item[$i]);
+        //         $field_nilai[$i] = array('name'=>'nilai[]','contents'=>intval(str_replace('.','', $request->nilai[$i])));
+        //         $field_ppn[$i] = array('name'=>'nilai_ppn[]','contents'=>intval(str_replace('.','', $request->nilai_ppn[$i])));
+        //         $field_date[$i] = array('name'=>'due_date[]','contents'=>$tanggal_due);
+                
+        //     }
+        //     $send_data = array_merge($fields,$field_generate,$field_dokumen,$field_cust,$field_kontrak,$field_item,$field_nilai,$field_ppn,$field_date);
+        // }
         
         $cek = $request->file_dok;
         $fields_dok = array();
@@ -376,20 +321,20 @@ class TagihanMTController extends Controller
         }
         
         try { 
-                $client = new Client();
-                $response = $client->request('POST', $this->link.'tagihan-maintain-ubah?no_bukti='.$id,[
-                    'headers' => [
-                        'Authorization' => 'Bearer '.Session::get('token'),
-                        'Accept'     => 'application/json',
-                    ],
-                    'multipart' => $send_data
-                ]);
-                if ($response->getStatusCode() == 200) { // 200 OK
-                    $response_data = $response->getBody()->getContents();
+            $client = new Client();
+            $response = $client->request('POST', $this->link.'tagihan-maintain-ubah?no_bukti='.$id,[
+                'headers' => [
+                    'Authorization' => 'Bearer '.Session::get('token'),
+                    'Accept'     => 'application/json',
+                ],
+                'multipart' => $send_data
+            ]);
+            if ($response->getStatusCode() == 200) { // 200 OK
+                $response_data = $response->getBody()->getContents();
                     
-                    $data = json_decode($response_data,true);
-                    return response()->json(['data' => $data], 200);  
-                }
+                $data = json_decode($response_data,true);
+                return response()->json(['data' => $data], 200);  
+            }
 
         } catch (BadResponseException $ex) {
                 $response = $ex->getResponse();
