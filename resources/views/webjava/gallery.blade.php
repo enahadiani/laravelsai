fb9cff5bf3198ddd54d512689f2263102d75c811
enahadiani <=>
2020-09-01 13:15:32 +0700

    update javaturbine

--- resources/views/webjava/gallery.blade.php
+++ resources/views/webjava/gallery.blade.php
@@ -10,6 +10,7 @@
     </div>
 </section><!--/#portfolio-item-->
 <script>
+
 function loadGallery()
 {
     $.ajax({
@@ -58,6 +59,20 @@ function loadGallery()
             }
             
             $('.portfolio-items').html('<center>'+html+'</center>');
+            var $portfolio_selectors = $('.portfolio-filter >li>a');
+            var $portfolio = $('.portfolio-items');
+            $portfolio.isotope({
+                itemSelector : '.portfolio-item',
+                layoutMode : 'fitRows'
+            });
+            
+            $portfolio_selectors.on('click', function(){
+                $portfolio_selectors.removeClass('active');
+                $(this).addClass('active');
+                var selector = $(this).attr('data-filter');
+                $portfolio.isotope({ filter: selector });
+                return false;
+            });
 
         },
         fail: function(xhr, textStatus, errorThrown){
