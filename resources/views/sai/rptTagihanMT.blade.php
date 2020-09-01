e34a2820c71eebcacdb4e313663d8a9672289982
oktapascal <oktaiscool@gmail.com>
2020-09-01 11:45:30 +0700

    Fix print pemberitahuan tagihan SAI

--- resources/views/sai/rptTagihanMT.blade.php
+++ resources/views/sai/rptTagihanMT.blade.php
@@ -129,16 +129,16 @@ drawLap($formData);
                 if(result.status) {
                     var date = new Date();
                     var html = '';
-                    html += `<div id="printPreview" style="margin-top: 70px;">
-                    <section id="tanggal-surat">
+                    html += `<div id="printPreview" style="margin-top: 70px;padding-left:30px;">
+                    <div id="tanggal-surat" style="position:fixed;">
                     <table>
                     <tr>
                         <td>Bandung, ${date.getDate()} ${result.data.bulan_tagihan} ${date.getFullYear()}</td>
                     </tr>
                     </table>
-                </section>`;
+                </div>`;
                     html += `
-                    <section id="nomor-surat" style="margin-top: 30px;">
+                    <div id="nomor-surat" style="margin-top: 40px;position:fixed;">
                     <table>
                     <tr>
                         <td>Nomor</td>
@@ -156,9 +156,9 @@ drawLap($formData);
                         <td>1 (Satu) bundel</td>
                     </tr>
                     </table>
-                </section>
+                </div>
 
-                <section id="alamat-tujuan" style="margin-top: 40px;">
+                <div id="alamat-tujuan" style="margin-top: 120px;position:fixed;">
                     <table>
                     <tr>
                         <td>Kepada Yth,</td>
@@ -176,35 +176,155 @@ drawLap($formData);
                         <td>${result.data.data[0].provinsi}</td>
                     </tr>
                     </table>
-                </section>`;
+                </div>`;
                 var tgl = result.data.data[0].tgl_sepakat;
                 var split = tgl.split('-');
-                html += `<section id="isi-surat" style="margin-top: 40px;">
-                    <table style="width: 100%;">
+                html += `<div id="isi-surat-1" style="margin-top: 240px;position:fixed;">
+                    <table style="width:100%;">
                         <tr>
                             <td>Dengan hormat,</td>
                         </tr>
-                        </table>
-                        <table style="width: 100%;">
+                    </table>
+                </div>`;
+                html += `<div id="isi-surat-2" style="margin-top: 275px;position:fixed;">
+                    <table style="width:95%;">
                         <tr>
-                            <td style="padding: 10px 15px 15px 0px; width: 5%;">1.</td>
-                            <td style="width: 80%; text-align: justify; padding-top: 10px;">
-                            Menunjuk Perjanjian Kerja Sama Nomor : ${result.data.data[0].no_dokumen} tanggal
-                            ${split[2]} ${result.data.bulan_sepakat} ${split[0]} perihal ${result.data.data[0].keterangan}.
+                            <td style="width: 5%;" valign="top">1.</td>
+                            <td style="text-align: justify;">
+                             Menunjuk Perjanjian Kerja Sama Nomor : ${result.data.data[0].no_dokumen} tanggal
+                             ${split[2]} ${result.data.bulan_sepakat} ${split[0]} perihal ${result.data.data[0].keterangan}.
                             </td>
                         </tr>
                     </table>
-                </section>
-                `;
-                html += `<table style="margin-top: -90px; width: 100%;"><tr>`;
-                html += `<td style="padding: 0px 15px 15px 0px; width: 5%;">2.</td>`;
-                html += `<td style="width: 80%; text-align: justify; padding-top: 95px;">`;
-                html += `Sehubungan dengan hal di atas, maka kami mengajukan permohonan
-                        pembayaran sebesar
-                        <b>Rp. ${sepNum(result.data.data[0].nilai_akhir)},- (${result.data.terbilang})</b> termasuk PPN
-                        10% dengan perincian sebagai berikut :`;
-                html += `</td>`;
-                html += `</tr></table>`;
+                </div>`;
+                html += `<div id="isi-surat-3" style="margin-top: 345px;position:fixed;">
+                    <table style="width:95%;">
+                        <tr>
+                            <td style="width: 5%;" valign="top">2.</td>
+                            <td style="text-align: justify;">
+                                Sehubungan dengan hal di atas, maka kami mengajukan permohonan pembayaran sebesar
+                                <b>Rp. ${sepNum(result.data.data[0].nilai_akhir)},- (${result.data.terbilang})</b> termasuk PPN
+                                10% dengan perincian sebagai berikut :
+                            </td>
+                        </tr>
+                    </table>
+                </div>`;
+                html += `<div id="isi-surat-4" style="margin-top: 410px;margin-left:35px;position:fixed;">
+                    <table style="width:100%;border: 1px solid black;border-collapse: collapse;">
+                        <thead>
+                            <tr>
+                                <th style="background-color: ghostwhite; width: 70%;border: 1px solid black;border-collapse: collapse;">
+                                    KETERANGAN
+                                </th>
+                                <th style="background-color: ghostwhite; width: 30%;border: 1px solid black;border-collapse: collapse;">
+                                    NILAI
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td style="border: 1px solid black;border-collapse: collapse;padding:3px;">
+                                    Biaya Sewa Aplikasi Keuangan Bulan ${result.data.bulan_tagihan}
+                                </td>
+                                <td style="border: 1px solid black;border-collapse: collapse;text-align:right;padding:3px;">
+                                    Rp. ${sepNum(result.data.data[0].nilai)}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="border: 1px solid black;border-collapse: collapse;padding:3px;">
+                                    PPN 10%
+                                </td>
+                                <td style="border: 1px solid black;border-collapse: collapse;text-align:right;padding:3px;">
+                                    Rp. ${sepNum(result.data.data[0].nilai_ppn)}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="border: 1px solid black;border-collapse: collapse;padding:3px;">
+                                    Jumlah Tagihan
+                                </td>
+                                <td style="border: 1px solid black;border-collapse: collapse;text-align:right;padding:3px;">
+                                    Rp. ${sepNum(result.data.data[0].nilai_akhir)}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>`;
+                html += `<div id="isi-surat-5" style="margin-top: 530px;position:fixed;">
+                    <table style="width:100%;">
+                        <tr>
+                            <td style="width: 5%;" valign="top">3.</td>
+                            <td style="text-align: justify;">
+                                Pembayaran mohon ditransfer ke :
+                                <table style="width:100%;">
+                                    <tr>
+                                        <td>Bank</td>
+                                        <td>:</td>
+                                        <td>${result.data.data_bank[0].bank}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Cabang</td>
+                                        <td>:</td>
+                                        <td>${result.data.data_bank[0].cabang}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>No. Rekening</td>
+                                        <td>:</td>
+                                        <td>${result.data.data_bank[0].no_rek}</td>
+                                    </tr>
+                                        <td>Atas Nama</td>
+                                        <td>:</td>
+                                        <td>${result.data.data_bank[0].nama_rek}</td>
+                                    </tr>
+                                    </tr>
+                                        <td colspan="3"><small>*Pembayaran maksimal ${result.data.data[0].due_date} hari setelah tagihan diterbitkan</small></td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </div>`;
+                var lamp = ``;
+                for(var i=0;i<result.data.data_lampiran.length;i++){
+                   lamp += `<tr>
+                        <td>${result.data.data_lampiran[i]}</td>
+                    </tr>`;
+                }
+                html += `<div id="isi-surat-6" style="margin-top: 675px;position:fixed;padding-bottom:20px;">
+                    <table style="width:100%;">
+                        <tr>
+                            <td style="width: 5%;" valign="top">4.</td>
+                            <td style="text-align: justify;">
+                            Untuk melengkapi surat permohonan ini, kami lampirkan pula dokumen sebagai berikut :
+                            <table>
+                           `+lamp+
+                           `</table>
+                           </td>
+                        </tr>
+                    </table>
+                </div>`;
+                html += `<div id="isi-surat-7" style="margin-top: 800px;position:fixed;">
+                    <table style="width:100%;">
+                        <tr>
+                            <td style="width: 5%;" valign="top">5.</td>
+                            <td style="text-align: justify;">
+                                Demikian kami sampaikan, atas perhatian dan kerja samanya kami ucapkan terima kasih.
+                            </td>
+                        </tr>
+                    </table>
+                </div>`;
+                html += `<div id="isi-surat-8" style="margin-top: 850px;position:fixed;">
+                    <table style="width:100%;">
+                        <tr>
+                            <td>Hormat kami,</td>
+                        </tr>
+                        <tr>
+                            <td style="padding-top: 50px;">
+                                <b style="text-decoration: underline;">Gamal Abdul Gani</b>
+                                <p style="margin-top: -3px;"><b>Direktur</b></p>
+                            </td>
+                        </tr>
+                    </table>
+                </div>`;
                 html += `</div>`;
                 var winPrint = window.open('', '', 'left=0,top=0,width=800,height=600,toolbar=0,scrollbars=0,status=0');
                 winPrint.document.write(html);
