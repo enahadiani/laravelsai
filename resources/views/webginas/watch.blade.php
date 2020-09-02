85e228aa36d7b59fde7ecdd4628cde5bf7caf3a7
enahadiani <=>
2020-09-02 09:10:11 +0700

    update trengginas

--- resources/views/webginas/watch.blade.php
+++ resources/views/webginas/watch.blade.php
@@ -14,11 +14,11 @@
         </div>
 
         <br><br>
-
-        <center>
-            <div class="row" id="keterangan" style="text-align: justify; text-justify: inter-word; width:95%;">
-            </div>
-        </center>
+        
+        <div class="row" id="keterangan" style="text-align: justify; text-justify: inter-word; width:95%;">
+            <center>
+            </center>
+        </div>
     </div>
 </section>
 @php
@@ -58,10 +58,10 @@ function loadWatch($nid)
                     var link = tmp[1];
                 }
                 $('#judul').html(line.judul);
-                $('#keterangan').html(line.keterangan);
+                $('#keterangan center').html(line.keterangan);
                 html+="<iframe width='90%' height='50%' style='width:80%; height:50%; min-height:280px; min-width:280px; max-width:560px;' src='"+(line.file_type == 'youtube' ? "https://www.youtube.com/embed/"+link : line.link )+"'></iframe>";
             }
-            $('#content-video').html(html);
+            $('#content-video center').html(html);
 
         },
         fail: function(xhr, textStatus, errorThrown){
