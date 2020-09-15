52af3118dc3daa1d57cd04f3c951f3f3baae34e3
oktapascal <oktaiscool@gmail.com>
2020-09-15 12:06:20 +0700

    Update routes master Yakes

---
+++ app/Http/Controllers/Yakes/JurnalSesuaiController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Yakes;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Session;
+use GuzzleHttp\Exception\BadResponseException;
+
+class JurnalSesuaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function __contruct(){
+        if(!Session::get('login')){
+            return redirect('yakes-auth/login');
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index(){
+        try {
+            $client = new Client();
+            $response = $client->request('GET',  config('api.url').'yakes-master/index',[
+                'headers' => [
+                    'Authorization' => 'Bearer '.Session::get('token'),
+                    'Accept'     => 'application/json',
+                ]
+            ]);
+
+            if ($response->getStatusCode() == 200) { // 200 OK
+                $response_data = $response->getBody()->getContents();
+                
+                $data = json_decode($response_data,true);
+                $data = $data["success"]["data"];
+            }
+            return response()->json(['daftar' => $data, 'status'=>true], 200); 
+
+        } catch (BadResponseException $ex) {
+            $response = $ex->getResponse();
+            $res = json_decode($response->getBody(),true);
+            return response()->json(['message' => $res["message"], 'status'=>false], 200);
+        }
+    }
+
+    public function getData($id) {}
+
+    public function update(Request $request, $id) {}
+
+    public function delete($id) {}
+   
+}
--- routes/yakes/trans.php
+++ routes/yakes/trans.php
@@ -2,3 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Session;
+
+// Data Jurnal Sesuai //
+Route::get('jurnal-sesuai', 'Yakes\JurnalSesuaiController@index');
