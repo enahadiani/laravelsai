410097d565d16d4d099c4b50f98b8466a9773efd
enahadiani <=>
2020-09-29 11:17:39 +0700

    update webginas

--- app/Http/Controllers/Webginas/WebController.php
+++ app/Http/Controllers/Webginas/WebController.php
@@ -40,6 +40,34 @@ class WebController extends Controller
         
     }
 
+    public function showView($param){
+
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $agen = getenv('HTTP_USER_AGENT');
+        $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
+        if($agen != false){
+            $client = new Client();
+            date_default_timezone_set('Asia/Jakarta');
+            $ins = array(
+                'nik'=>'visitor',
+                'tanggal' => date('Y-m-d H:i:s'),
+                'ip' => $ip,
+                'agen' => $agen,
+                'kota' => $details->city,
+                'loc' => $details->loc,
+                'region' => $details->region,
+                'negara' => $details->country,
+                'kode_lokasi' => '17',
+                'kode_pp' => '-',
+                'page' => $param['menu']
+            );
+            $response = $client->request('POST',  config('api.url').'webginas/lab-log/webginas',[
+                'form_params' => $ins
+            ]);
+        }
+        return view($param['view'],$param['data']);
+    }
+
     public function cek_session()
     {
         // return view('login');
