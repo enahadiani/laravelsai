cafde343fff4f6ae7425bed6f85fab6a3cfd742e
enahadiani <=>
2020-10-14 11:02:07 +0700

    update sekolah

---
+++ app/Http/Controllers/Sekolah/LaporanController.php
@@ -0,0 +1,96 @@
+<?php
+    namespace App\Http\Controllers\Sekolah;
+
+    use App\Http\Controllers\Controller;
+    use Illuminate\Support\Facades\DB;
+    use GuzzleHttp\Client;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Session;
+    use GuzzleHttp\Exception\BadResponseException;
+
+    class LaporanController extends Controller {
+
+
+        public function __contruct() {
+            if(!Session::get('login')){
+            return redirect('sekolah-auth/login');
+            }
+        }
+    
+        function getNilai(Request $request){
+            try{
+    
+                $client = new Client();
+        
+                $response = $client->request('GET',  config('api.url').'sekolah/lap-nilai',[
+                    'headers' => [
+                        'Authorization' => 'Bearer '.Session::get('token'),
+                        'Accept'     => 'application/json',
+                    ],
+                    'query' => [
+                        'kode_pp' => $request->kode_pp,
+                        'kode_ta' => $request->kode_ta,
+                        'kode_kelas' => $request->kode_kelas,
+                        'kode_matpel' => $request->kode_matpel
+                    ]
+                ]);
+        
+                if ($response->getStatusCode() == 200) { // 200 OK
+                    $response_data = $response->getBody()->getContents();
+                    
+                    $res = json_decode($response_data,true);
+                    $data = $res["data"];
+                    $detail = $res["data_detail"];
+                }
+                if(isset($request->back)){
+                    $back = true;
+                }else{
+                    $back = false;
+                }
+                
+                return response()->json(['result' => $data, 'status'=>true, 'auth_status'=>1, 'detail'=>$detail,'res'=>$res,'lokasi'=>Session::get('namaLokasi'),'back'=>$back], 200); 
+            } catch (BadResponseException $ex) {
+                $response = $ex->getResponse();
+                $res = json_decode($response->getBody(),true);
+                return response()->json(['message' => $res["message"], 'status'=>false, 'auth_status'=>2], 200);
+            } 
+           
+        }
+
+        function sendMail(Request $request){
+            try{
+                
+                $client = new Client();
+                
+                $query = [
+                    'periode' => $request->periode,
+                    'kode_akun' => $request->kode_akun,
+                    'email' => $request->email
+                ];
+        
+                $response = $client->request('POST',  config('api.url').'sekolah/send-laporan',[
+                    'headers' => [
+                        'Authorization' => 'Bearer '.Session::get('token'),
+                        'Accept'     => 'application/json',
+                        'Content-Type'     => 'application/json',
+                    ],
+                    'body' => json_encode($query)
+                ]);
+        
+                if ($response->getStatusCode() == 200) { // 200 OK
+                    $response_data = $response->getBody()->getContents();
+                    
+                    $res = json_decode($response_data,true);
+                    $data = $res;
+                }
+
+                return response()->json($data, 200);    
+            } catch (BadResponseException $ex) {
+                $response = $ex->getResponse();
+                $res = json_decode($response->getBody(),true);
+                return response()->json(['message' => $res["message"], 'status'=>false], 200);
+            } 
+        }
+
+    }
+?>
---
+++ resources/views/sekolah/rptNilai.blade.php
@@ -0,0 +1,93 @@
+<script type="text/javascript">
+    function drawLap(formData){
+        saiPostLoad('sekolah-report/lap-nilai', null, formData, null, function(res){
+           if(res.result.length > 0){
+
+                $('#pagination').html('');
+                var show = $('#show').val();
+                generatePaginationDore('pagination',show,res);
+              
+           }else{
+                $('#saku-report #canvasPreview').load("{{ url('sekolah-auth/form/blank') }}");
+           }
+       });
+   }
+
+   drawLap($formData);
+
+   function drawRptPage(data,res,from,to){
+        var data = data;
+        if(data.length > 0){
+            if(res.back){
+                $('.navigation-lap').removeClass('hidden');
+            }else{
+                $('.navigation-lap').addClass('hidden');
+            }
+            var html = `<div align='center'>
+            <style>
+                .info-table thead{
+                    // background:#e9ecef;
+                }
+                .no-border td{
+                    border:0 !important;
+                }
+                .bold {
+                    font-weight:bold;
+                }
+            </style>
+
+            `;
+            var lokasi = res.lokasi;
+            html+=judul_lap("LAPORAN PENILAIAN",lokasi,'Periode '+periode.fromname)+`
+                    <table class='table table-bordered' width='100%'>
+                        <tr>
+                            <td class='text-center;' width='35%'>Deskripsi</td>
+                            <td class='text-center;' width='15%'>Jumlah</td>
+                            <td class='text-center;' width='35%'>Deskripsi</td>
+                            <td class='text-center;' width='15%'>Jumlah</td>
+                        </tr>
+                `;
+                    var det = "";
+                    for (var i=0; i < data.length ; i++)
+                    {
+                        var line  = data[i];
+                        var nilai1 = "";
+                        var nilai2 = "";
+                        if (line.tipe1 != "Header" && line.nama1 != "." && line.nama1 != "")
+                        {
+                            nilai1=sepNum(parseFloat(line.nilai1));
+                        }
+                        if (line.tipe2 != "Header" && line.nama2 != "." && line.nama2 != "")
+                        { 
+                            nilai2=sepNum(parseFloat(line.nilai2));
+                        }
+                        det +="<tr>";
+                        if (line.tipe1 == "Posting" && line.nilai1 != 0)
+                        {
+                            det += "<td valign='middle' class='isi_laporan report-link neraca-lajur link-report' style='cursor:pointer;' data-kode_neraca='"+line.kode_neraca1+"'>"+fnSpasi(line.level_spasi1)+line.nama1+"</td>";
+                        }
+                        else
+                        {
+                            det += "<td valign='middle' class='isi_laporan '>"+fnSpasi(line.level_spasi1)+line.nama1+"</td>";
+                        }
+                        det +=`<td valign='middle' class='isi_laporan' align='right'>`+nilai1+`</td>`;
+
+                         if (line.tipe2 == "Posting" && line.nilai2 != 0)
+                        {
+                            det += "<td valign='middle' class='isi_laporan report-link neraca-lajur link-report' style='cursor:pointer;' data-kode_neraca='"+line.kode_neraca2+"'>"+fnSpasi(line.level_spasi2)+line.nama2+"</td>";
+                        }
+                        else
+                        {
+                            det += "<td valign='middle' class='isi_laporan '>"+fnSpasi(line.level_spasi2)+line.nama2+"</td>";
+                        }
+                        det +="<td valign='middle' class='isi_laporan' align='right'>"+nilai2+"</td></tr>";
+                    }
+            html+=det+`</table>
+            </div>`;
+        }
+        $('#canvasPreview').html(html);
+        $('li.prev a ').html("<i class='simple-icon-arrow-left'></i>");
+        $('li.next a ').html("<i class='simple-icon-arrow-right'></i>");
+    }
+</script>
+   
