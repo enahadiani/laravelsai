cb32701e99d95a763f0a4d1b5dbddc9fafaead8f
enahadiani <=>
2020-10-14 10:50:54 +0700

    update sekolah

--- routes/sekolah/report.php
+++ routes/sekolah/report.php
@@ -2,3 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Session;
+
+Route::get('filter-pp','Sekolah\FilterController@getFilterPP');
+Route::get('filter-ta','Sekolah\FilterController@getFilterTA');
+Route::get('filter-kelas','Sekolah\FilterController@getFilterKelas');
+Route::get('filter-matpel','Sekolah\FilterController@getFilterMatpel');
+
