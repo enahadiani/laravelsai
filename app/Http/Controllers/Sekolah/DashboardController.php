d985c20fbc4af0c1812c55fc7d937b14438995c8
oktapascal <oktaiscool@gmail.com>
2020-10-16 10:32:21 +0700

    Dashboard Controller Sekolah

---
+++ app/Http/Controllers/Sekolah/DashboardController.php
@@ -0,0 +1,52 @@
+<?php
+    namespace App\Http\Controllers\Sekolah;
+
+    use App\Http\Controllers\Controller;
+    use Illuminate\Http\Request;
+    use GuzzleHttp\Client;
+    use Illuminate\Support\Facades\Session;
+    use GuzzleHttp\Exception\BadResponseException;
+
+    class DashboardController extends Controller {
+
+        public function __contruct() {
+            if(!Session::get('login')){
+            return redirect('tarbak/login');
+            }
+        }
+
+        public function rata2Nilai(Request $request)
+        {
+            try{
+
+                $client = new Client();
+                $response = $client->request('GET',  config('api.url').'sekolah/rata2-nilai',[
+                    'headers' => [
+                        'Authorization' => 'Bearer '.Session::get('token'),
+                        'Accept'     => 'application/json',
+                    ],
+                    'query' => [
+                        'kode_pp' => $request->kode_pp,
+                        'kode_kelas' => $request->kode_kelas,
+                        'kode_matpel' => $request->kode_matpel
+                    ]
+                ]);
+    
+                if ($response->getStatusCode() == 200) { // 200 OK
+                    $response_data = $response->getBody()->getContents();
+                
+                    $data = json_decode($response_data,true);
+                    $data = $data["success"];
+                }
+                return response()->json(['data' => $data], 200); 
+            } catch (BadResponseException $ex) {
+                $response = $ex->getResponse();
+                $res = json_decode($response->getBody(),true);
+                $data['message'] = $res;
+                $data['status'] = false;
+                return response()->json(['data' => $data], 200);
+            }
+        }
+
+    }
+?>
