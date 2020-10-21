6fa89904ec08a4bb7e125c3c91b65218cc3fdcd0
enahadiani <=>
2020-10-21 15:16:00 +0700

    update sekolah

--- resources/views/sekolah/dashSiswa.blade.php
+++ resources/views/sekolah/dashSiswa.blade.php
@@ -1,207 +1,5 @@
     <!-- CSS tambahan -->
     <style>
-        th,td{
-            padding:8px !important;
-            vertical-align:middle !important;
-        }
-        input.error{
-            border:1px solid #dc3545;
-        }
-        label.error{
-            color:#dc3545;
-            margin:0;
-        }
-        #table-data_paginate,#table-search_paginate
-        {
-            margin-top:0 !important;
-        }
-
-        #table-data_paginate ul,#table-search_paginate ul
-        {
-            float:right;
-        }
-        .form-body 
-        {
-            position: relative;
-            overflow: auto;
-        }
-
-        #content-delete
-        {
-            position: relative;
-            overflow: auto;
-        }
-        
-        #table-search
-        {
-            border-collapse:collapse !important;
-        }
-
-        .hidden{
-            display:none;
-        }
-
-        #table-search_filter label, #table-search_filter input
-        {
-            width:100%;
-        }
-
-        .dataTables_wrapper .paginate_button.previous {
-        margin-right: 0px; }
-
-        .dataTables_wrapper .paginate_button.next {
-        margin-left: 0px; }
-
-        div.dataTables_wrapper div.dataTables_paginate {
-        margin-top: 25px; }
-
-        div.dataTables_wrapper div.dataTables_paginate ul.pagination {
-        justify-content: center; }
-
-        .dataTables_wrapper .paginate_button.page-item {
-            padding-left: 5px;
-            padding-right: 5px; 
-        }
-
-        .dataTables_length select {
-            border: 0;
-            background: none;
-            box-shadow: none;
-            border:none;
-            width:120px !important;
-            transition-duration: 0.3s; 
-        }
-
-        #table-data_filter label
-        {
-            width:100%;
-        }
-        #table-data_filter label input
-        {
-            width:inherit;
-        }
-        #searchData
-        {
-            font-size: .75rem;
-            height: 31px;
-        }
-        .dropdown-toggle::after {
-            display:none;
-        }
-        .dropdown-aksi > .dropdown-item{
-            font-size : 0.7rem;
-        }
-        .last-add::before{
-            content: "***";
-            background: var(--theme-color-1);
-            border-radius: 50%;
-            font-size: 3px;
-            position: relative;
-            top: -2px;
-            left: -5px;
-        }
-
-        div.dataTables_wrapper div.dataTables_filter input{
-            height:calc(1.3rem + 1rem) !important;
-        }
-
-        .input-group-prepend{
-            border-top-left-radius: 0.5rem;
-            border-bottom-left-radius: 0.5rem;
-        }
-
-        .readonly > .input-group-prepend{
-            background: #e9ecef !important;
-        }
-
-        .readonly > .search-item2{
-            background: #e9ecef !important;
-            cursor:not-allowed;
-            display:none;
-        }
-
-        .input-group > .form-control 
-        {
-            border-radius: 0.5rem !important;
-        }
-
-        .input-group-prepend > span {
-            margin: 5px;padding: 0 5px;
-            background: #e9ecef !important;
-            border: 1px solid #e9ecef !important;
-            border-radius: 0.25rem !important;
-            color: var(--theme-color-1);
-            font-weight:bold;
-            cursor:pointer;
-        }
-
-        .readonly > .input-group-prepend > span {
-            margin: 5px;padding: 0 5px;
-            background: #d7d7d7 !important;
-            border: 1px solid #d7d7d7 !important;
-            border-radius: 0.25rem !important;
-            color: black;
-            font-weight:bold;
-            cursor:pointer;
-        }
-
-        span[class^=info-name]{
-            cursor:pointer;font-size: 12px;position: absolute; top: 3px; left: 52.36663818359375px; padding: 5px 0px 5px 5px; z-index: 2; width: 180.883px;background:white;
-            overflow: hidden;
-            white-space: nowrap;
-            text-overflow: ellipsis;
-            line-height:22px;
-        }
-
-        .readonly > span[class^=info-name] {
-            cursor:pointer;font-size: 12px;position: absolute; top: 3px; left: 52.36663818359375px; padding: 5px 0px 5px 5px; z-index: 2; width: 180.883px;background:white;
-            overflow: hidden;
-            white-space: nowrap;
-            text-overflow: ellipsis;
-            line-height:22px;
-            background: #e9ecef !important;
-
-        }
-
-        .info-icon-hapus{
-            font-size: 14px;
-            position: absolute;
-            top: 10px;
-            right: 35px;
-            z-index: 3;
-        }
-
-        .readonly >  .info-icon-hapus{
-            display:none;
-        }
-
-        .form-control {
-            padding: 0.1rem 0.5rem; 
-            height: calc(1.3rem + 1rem);
-            border-radius:0.5rem;
-            
-        }
-
-        .readonly >  .form-control{
-            background: #e9ecef !important;
-        }
-
-        .selectize-input {
-            min-height: unset !important;
-            padding: 0.1rem 0.5rem; 
-            height: calc(1.3rem + 1rem);
-            line-height: 30px;
-            border-radius: 0.5rem;
-        }
-
-        label{
-            margin-bottom: 0.2rem;
-        }
-
-        .search-item2{
-            cursor:pointer;
-            font-size: 16px;margin-left:5px;position: absolute;top: 5px;right: 10px;background: white;padding: 5px 0 5px 5px;z-index: 4;height:27px;
-        }
         .kop img.logo{
             width:100px !important;
             height:100px !important;
@@ -212,86 +10,97 @@
         .menu-icon{
             cursor:pointer;
         }
+
+        #profile-table td,th
+        {
+            padding:0px !important;
+        }
+
     </style>
-    <div class="card" id="saku-dashboard">
-        <div class="card-body">
-            <div class="row mb-4">
-                <div class="text-center col-sm-3 col-4 menu-icon">
-                    <img alt="Profile" src="{{ asset('img/menu-siswa/Buku Penghubung.png') }}" class="img-thumbnail border-0 rounded-circle mb-1 list-thumbnail">
-                    <p hidden class="kode_form">fBukuPenghubung</p>
-                    <p class="list-item-heading mb-3">Buku Penghubung</p>
-                </div>
-                <div class="text-center col-sm-3 col-4 menu-icon">
-                    <img alt="Profile" src="{{ asset('img/menu-siswa/Keuangan.png') }}" class="img-thumbnail border-0 rounded-circle mb-1 list-thumbnail">
-                    <p hidden class="kode_form">fKeuangan</p>
-                    <p class="list-item-heading mb-3">Keuangan</p>
-                </div>
-                <div class="text-center col-sm-3 col-4 menu-icon">
-                    <img alt="Profile" src="{{ asset('img/menu-siswa/Kalender Akademik.png') }}" class="img-thumbnail border-0 rounded-circle mb-1 list-thumbnail">
-                    <p hidden class="kode_form">fKalenderAkademik</p>
-                    <p class="list-item-heading mb-3">Kalender Akademik</p>
-                </div>
-                <div class="text-center col-sm-3 col-4 menu-icon">
-                    <img alt="Profile" src="{{ asset('img/menu-siswa/Absensi.png') }}" class="img-thumbnail border-0 rounded-circle mb-1 list-thumbnail">
-                    <p hidden class="kode_form">fAbsensi</p>
-                    <p class="list-item-heading mb-3">Absensi</p>
-                </div>
-                <div class="text-center col-sm-3 col-4 menu-icon">
-                    <img alt="Profile" src="{{ asset('img/menu-siswa/Nilais.png') }}" class="img-thumbnail border-0 rounded-circle mb-1 list-thumbnail">
-                    <p hidden class="kode_form">fNilai</p>
-                    <p class="list-item-heading mb-3">Nilai</p>
-                </div>
-                <div class="text-center col-sm-3 col-4 menu-icon">
-                    <img alt="Profile" src="{{ asset('img/menu-siswa/Jadwal Pelajaran.png') }}" class="img-thumbnail border-0 rounded-circle mb-1 list-thumbnail">
-                    <p hidden class="kode_form">fJadwalPelajaran</p>
-                    <p class="list-item-heading mb-3">Jadwal Pelajaran</p>
-                </div>
-                <div class="text-center col-sm-3 col-4 menu-icon">
-                    <img alt="Profile" src="{{ asset('img/menu-siswa/Prestasis.png') }}" class="img-thumbnail border-0 rounded-circle mb-1 list-thumbnail">
-                    <p hidden class="kode_form">fPrestasi</p>
-                    <p class="list-item-heading mb-3">Prestasi</p>
-                </div>
-                <div class="text-center col-sm-3 col-4 menu-icon">
-                    <img alt="Profile" src="{{ asset('img/menu-siswa/Report.png') }}" class="img-thumbnail border-0 rounded-circle mb-1 list-thumbnail">
-                    <p hidden class="kode_form">fRaport</p>
-                    <p class="list-item-heading mb-3">Raport</p>
-                </div>
-                <div class="text-center col-sm-3 col-4 menu-icon">
-                    <img alt="Profile" src="{{ asset('img/menu-siswa/Ekstrakulikuler.png') }}" class="img-thumbnail border-0 rounded-circle mb-1 list-thumbnail">
-                    <p hidden class="kode_form">fEkskul</p>
-                    <p class="list-item-heading mb-3">Ekskul</p>
+        @if (Session::get('foto') == "" || Session::get('foto') == "-" )
+    <style>
+        #profile-photo
+        {
+            height:180px;
+            background-image:url("{{ asset('asset_elite/images/user.png') }}");
+            /* background-image:url("{{ asset('asset_dore/img/profiles/l-1.jpg') }}"); */
+            background-position: center; /* Center the image */
+            background-repeat: no-repeat; /* Do not repeat the image */
+            background-size: cover;
+            border-radius:0.75rem; 
+        }
+    </style>
+        @else
+    <style>
+        #profile-photo
+        {
+            
+            height:180px;
+            background-image:url("{{ config('api.url').'sekolah/storage/'.Session::get('foto') }}"); background-position: center; /* Center the image */
+            background-repeat: no-repeat; /* Do not repeat the image */
+            background-size: cover;
+            border-radius:0.75rem; 
+        }
+    </style>
+        @endif
+    <div class="row">
+        <div class="col-md-3 mb-3">
+            <div class="card">
+                <div class="card-body" id="profile-photo">
+                   
                 </div>
             </div>
-            <h5>Informasi</h5>
-            <div class="separator"></div>
-            <div class="row mt-4">
-                <div class="col-lg-2 col-md-4 col-12 mb-4">
-                    <div class="card">
-                        <div class="position-relative">
-                            <img class="card-img-top" src="{{ asset('asset_web/homepages/kegiatan/2.jpg') }}" alt="Card image cap">
-                        </div>
-                        <div class="card-body">
-                            <p class="list-item-heading mb-4">Homemade Cheesecake with Fresh Berries and Mint
-                            </p>
-                            <footer>
-                            <p class="text-muted text-small mb-0 font-weight-light">09.04.2018</p>
-                            </footer>
-                        </div>
-                    </div>
+        </div>
+        <div class="col-md-9 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5><b>Profile Siswa</b></h5>
+                    <table class="table no-margin table-borderless" id="profile-table">
+                        <tr>
+                            <td style="width:19%">NIS</td>
+                            <td style="width:1%">:</td>
+                            <td style="width:80%" id="nis"></td>
+                        </tr>
+                        <tr>
+                            <td>Nama</td>
+                            <td>:</td>
+                            <td id="nama"></td>
+                        </tr>
+                        <tr>
+                            <td>Jurusan</td>
+                            <td>:</td>
+                            <td id="nama_jur"></td>
+                        </tr>
+                        <tr>
+                            <td>Kelas</td>
+                            <td>:</td>
+                            <td id="kode_kelas"></td>
+                        </tr>
+                    </table>
                 </div>
-                <div class="col-lg-2 col-md-4 col-12 mb-4">
-                    <div class="card">
-                        <div class="position-relative">
-                            <img class="card-img-top" src="{{ asset('asset_web/homepages/kegiatan/3.jpg') }}" alt="Card image cap">
-                        </div>
-                        <div class="card-body">
-                            <p class="list-item-heading mb-4">Homemade Cheesecake with Fresh Berries and Mint
-                            </p>
-                            <footer>
-                            <p class="text-muted text-small mb-0 font-weight-light">09.04.2018</p>
-                            </footer>
-                        </div>
-                    </div>
+            </div>
+        </div>
+    </div>
+    <div class="card" id="saku-dashboard">
+        <div class="card-body">
+            <div class="row mb-4" style="justify-content:center">
+                <div class="col-sm-12 text-center mb-3">
+                    <h5><b>Layanan Siswa</b></h5>
+                </div>
+                <div class="text-center col-sm-3 col-4 menu-icon">
+                    <img alt="Profile" src="{{ asset('img/menu-siswa/check.png') }}" class="img-thumbnail border-0 rounded-circle mb-1 list-thumbnail">
+                    <p hidden class="kode_form">fKartuPiutang</p>
+                    <p class="list-item-heading mb-3">Kartu Siswa</p>
+                </div>
+                <div class="text-center col-sm-3 col-4 menu-icon">
+                    <img alt="Profile" src="{{ asset('img/menu-siswa/wallet.png') }}" class="img-thumbnail border-0 rounded-circle mb-1 list-thumbnail">
+                    <p hidden class="kode_form">fKartuPDD</p>
+                    <p class="list-item-heading mb-3">Kartu PDD</p>
+                </div>
+                <div class="text-center col-sm-3 col-4 menu-icon">
+                    <img alt="Profile" src="{{ asset('img/menu-siswa/report2.png') }}" class="img-thumbnail border-0 rounded-circle mb-1 list-thumbnail">
+                    <p hidden class="kode_form">fRaport</p>
+                    <p class="list-item-heading mb-3">Raport</p>
                 </div>
             </div>
         </div>
@@ -340,5 +149,41 @@
         var form = $(this).find('.kode_form').html();
         loadForm("{{url('sekolah-auth/form')}}/"+form);
     });
+
+    function getProfile(){
+        $.ajax({
+            type: "GET",
+            url: "{{ url('sekolah-dash/dash-siswa-profile') }}",
+            dataType: 'json',
+            data: {},
+            success:function(result){    
+                if(result.status){
+                    if(result.data.length > 0){
+                        var line = result.data[0];
+                        console.log(line);
+                        $('#nis').html(line.nis);
+                        $('#nama').html(line.nama);
+                        $('#kode_kelas').html(line.kode_kelas);
+                        $('#nama_jur').html(line.nama_jur);
+                    }
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {       
+                if(jqXHR.status == 422){
+                    var msg = jqXHR.responseText;
+                }else if(jqXHR.status == 500) {
+                    var msg = "Internal server error";
+                }else if(jqXHR.status == 401){
+                    var msg = "Unauthorized";
+                    window.location="{{ url('/sekolah-auth/sesi-habis') }}";
+                }else if(jqXHR.status == 405){
+                    var msg = "Route not valid. Page not found";
+                }
+                
+            }
+        });
+    }
+
+    getProfile();
 
     </script>
--- resources/views/sekolah/fKartuPiutang.blade.php
+++ resources/views/sekolah/fKartuPiutang.blade.php
@@ -207,12 +207,13 @@
             height:100px !important;
         }
     </style>
-    <div class="row" id="saku-datatable">
+    <div class="row" id="saku-dashboard">
         <div class="col-12">
             <div class="card mb-3">
                 <div class="card-body pb-3" style="padding-top:1rem;">
                     <h5 style="position:absolute;top: 25px;"></h5>
-                    <button type="button" id="btn-print" class="btn btn-primary" style="float:right;"><i class="simple-icon-printer mr-1"></i> Print</button>
+                    <button type="button" id="btn-print" class="btn btn-primary ml-2" style="float:right;"><i class="simple-icon-printer mr-1"></i> Print</button>
+                    <button type="button" id="btn-kembali" class="btn btn-light" style="float:right;"><i class="simple-arrow-left mr-1"></i> Kembali</button>
                 </div>
                
             </div>
@@ -231,7 +232,7 @@
                     <div class="separator my-1"></div>
                     <div class="kartu-m">
                         <div class="row">
-                            <div class="col-sm-12 text-center"><h5><b><u>KARTU SISWA</u></b></h5></div>
+                            <div class="col-sm-12 text-center"><h5><b><u>KARTU PIUTANG</u></b></h5></div>
                             <div class="col-sm-12"><h6><b>Identitas Siswa</b></h6></div>
                             <div class="col-sm-2">NIS</div>
                             <div class="col-sm-10" id="nis"></div>
@@ -314,7 +315,7 @@
     function getKartuPiutang(){
         $.ajax({
             type: "GET",
-            url: "{{ url('sekolah-trans/kartu-piutang') }}",
+            url: "{{ url('sekolah-dash/kartu-piutang') }}",
             dataType: 'json',
             data: {},
             success:function(result){    
@@ -386,12 +387,18 @@
     var scrollform = document.querySelector('.table-responsive');
     var psscrollform = new PerfectScrollbar(scrollform);
    
-    $('#saku-datatable').on('click','#btn-print',function(e){
+    $('#saku-dashboard').on('click','#btn-print',function(e){
         e.preventDefault();
         $('#print-area').printThis({
             removeInline: true,
             importStyle: true
         });
+    });
+
+    $('#saku-dashboard').on('click', '#btn-kembali', function(){
+        
+        var form ="{{ Session::get('dash') }}";
+        loadForm("{{ url('sekolah-auth/form') }}/"+form);
     });
 
     </script>
