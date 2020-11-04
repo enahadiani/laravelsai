687cb60cfd4e5e0a4aa9685cfcf53b601ec55229
oktapascal <oktaiscool@gmail.com>
2020-11-04 12:49:39 +0700

    Update header

--- resources/views/webginas/perusahaan.blade.php
+++ resources/views/webginas/perusahaan.blade.php
@@ -13,19 +13,33 @@
     <!-- Stylesheets & Fonts -->
     <link href="{{ asset('asset_web/css/plugins.css') }}" rel="stylesheet">
     <link href="{{ asset('asset_web/css/style.css') }}" rel="stylesheet">
+    <style>
+        h2::before{
+            background-color: #DD1F1A !important;
+        }
+        header.sticky-active > .header-inner > .container > #logo > a > span.name {
+            color: black !important;
+        }
+        .white {
+            color: white !important;
+        }
+        .black{
+            color: black !important;
+        }
+    </style>
 </head>
 
 <body>
     <!-- Body Inner -->
     <div class="body-inner">
         <!-- Header -->
-        <header id="header" data-fullwidth="true" data-transparent="true" class="submenu-light">
+        <header id="header" data-fullwidth="true" data-transparent="true">
             <div class="header-inner">
                 <div class="container">
                     <!--Logo-->
                     <div id="logo">
                         <a href="index.html">
-                            <span style="color: white !important;"><img src="{{ asset('asset_web/img/Trengginas@2x.png') }}" class="mr-2"> Trengginas</span>
+                            <span class="watch-class white"><img src="{{ asset('asset_web/img/Trengginas@2x.png') }}" class="mr-2"> Trengginas</span>
                         </a>
                     </div>
                     <!--End: Logo-->
@@ -47,8 +61,8 @@
                         <div class="container">
                             <nav>
                                 <ul>
-                                    <li><a href="#" class="a_link" data-href="fHome" style="color: white;">Home</a></li>
-                                    <li class="dropdown mega-menu-item"><a href="#" class="a_link" data-href="fLayanan" style="color: white;">Layanan</a>
+                                    <li><a href="#" class="a_link watch-class white" data-href="fHome">Home</a></li>
+                                    <li class="dropdown mega-menu-item"><a href="#" class="a_link watch-class white" data-href="fLayanan">Layanan</a>
                                         <ul class="dropdown-menu">
                                             <li class="mega-menu-content">
                                                 <div class="row">
@@ -84,8 +98,8 @@
                                             </li>
                                         </ul>
                                     </li>
-                                    <li><a href="{{url('/webginas2/perusahaan')}}" class="a_link" data-href="fPerusahaan" style="color: white;">Perusahaan</a></li>
-                                    <li><a href="#" class="a_link" data-href="fKontak" style="color: white;">Kontak</a></li>
+                                    <li><a href="{{url('/webginas2/perusahaan')}}" class="a_link watch-class white" data-href="fPerusahaan">Perusahaan</a></li>
+                                    <li><a href="#" class="a_link watch-class white" data-href="fKontak">Kontak</a></li>
                                 </ul>
                             </nav>
                         </div>
@@ -96,7 +110,7 @@
         </header>
         <!-- end: Header -->
         <!-- Page title -->
-        <section id="page-title" class="page-title-center text-light" style="background-color:#990000;">
+        <section id="page-title" class="page-title-center text-light" style="background-color:#DD1F1A;">
             <div class="bg-overlay"></div>
             <div class="container">
                 <div class="page-title">
@@ -115,9 +129,87 @@
         <!-- end: Page title -->
         <!-- Content -->
         
+        <!-- About -->
+        <section>
+            <div class="container">
+                <div class="heading-text heading-section m-b-80">
+                    <h2>Tentang Kami</h2>
+                </div>
+            </div>
+        </section>
+        <!-- End About -->
+        
+        <!-- Contact -->
+        <section class="p-t-100 background-grey p-b-200" style="background-position:71% 22%;">
+             <div class="container">
+                 <div class="row">
+                     <div class="col-lg-6">
+                         <div class="row">
+                             <div class="col-lg-12">
+                                 <h2 class="m-b-10">Hubungi Kami</h2>
+                             </div>
+                             <div class="col-lg-6 m-b-30">
+                                 <address>
+                                     <strong>Alamat:</strong><br>
+                                     Jl. Sumur Bandung No. 12, Bandung<br>
+                                 </address>
+                                 <strong>Telp:</strong> (022) 253205
+                                 <br>
+                                 <strong>Fax:</strong> (022) 2532053
+                                 <br>
+                                 <strong>Email:</strong> trengginasjaya@yahoo.co.id
+                             </div>
+                             <div class="col-lg-12 m-b-30">
+                                 <h4>Sosial Media</h4>
+                                 <div class="social-icons social-icons-light social-icons-colored-hover">
+                                     <ul>
+                                         <li class="social-facebook"><a href="#"><i class="fab fa-facebook-f"></i></a>
+                                         </li>
+                                         <li class="social-twitter"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                         <li class="social-instagram"><a href="#"><i class="fab fa-instagram"></i></a>
+                                         <li class="social-youtube"><a href="#"><i class="fab fa-youtube"></i></a></li>
+                                         </li>
+                                     </ul>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="col-lg-5 offset-1">
+                         <form class="widget-contact-form" novalidate action="include/contact-form.php" role="form" method="post">
+                             <div class="row">
+                                 <div class="form-group col-md-6">
+                                     <label for="name">Nama</label>
+                                     <input type="text" aria-required="true" required name="widget-contact-form-name" class="form-control required name" >
+                                 </div>
+                                 <div class="form-group col-md-6">
+                                     <label for="email">Email</label>
+                                     <input type="email" aria-required="true" required name="widget-contact-form-email" class="form-control required email" >
+                                 </div>
+                             </div>
+                             <div class="form-group">
+                                 <label for="message">Pesan</label>
+                                 <textarea type="text" required name="widget-contact-form-message" rows="8" class="form-control required" ></textarea>
+                             </div>
+                             <div class="form-group">
+                                 <button class="btn bg-red" type="submit" id="form-submit">Kirim Pesan</button>
+                             </div>
+                         </form>
+
+                     </div>
+                 </div>
+             </div>
+         </section>
+         <!-- end: Contact -->
+
         <!-- end: Content -->
         <!-- Footer -->
-
+        <footer id="footer">
+            <div class="copyright-content">
+                <div class="container">
+                    <div class="copyright-text text-center">&copy; 2020 PT Trengginas Jaya. All Rights Reserved.</div>
+                </div>
+            </div>
+        </footer>
         <!-- end: Footer -->
     </div>
     <!-- end: Body Inner -->
@@ -128,6 +220,18 @@
     <script src="{{ asset('asset_web/js/plugins.js') }}"></script>
     <!--Template functions-->
     <script src="{{ asset('asset_web/js/functions.js') }}"></script>
+    <script type="text/javascript">
+        function checkHeader(){
+            if($('#header').hasClass('sticky-active')) {
+                $('.watch-class').removeClass('white');
+                $('.watch-class').addClass('black');                
+            } else {
+                $('.watch-class').removeClass('black');
+                $('.watch-class').addClass('white');
+            }
+        }
+        setInterval(checkHeader, 500);
+    </script>
 </body>
 
 </html>
