bc8ea40a83eaea08278a752ac9f43b7352fba667
enahadiani <=>
2020-11-04 15:20:46 +0700

    delete component

--- resources/views/components/sai-text.blade.php
+++
@@ -1,5 +0,0 @@
-<label for="{{ $id }}">{{ $label }}</label>
-<textarea class="form-control {{ $class }}" id="{{ $id }}" name="{{ $name }}" {{ $attr }}  value="{{ $value }}"></textarea>
-@if(isset($icon))
-    <i style="font-size: 18px;position: absolute;top: 35px;right: 25px;" class="{{ $icon['class'] }} date-search"></i>
-@endif
