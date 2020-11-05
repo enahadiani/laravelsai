b39cfbd3f982d6ba4c995478eb155c50e15dfada
enahadiani <=>
2020-11-05 11:33:08 +0700

    update laporan

--- resources/views/sekolah/fLapGuruMatpel.blade.php
+++ resources/views/sekolah/fLapGuruMatpel.blade.php
@@ -2,22 +2,7 @@
     <div class="row" id="saku-filter">
         <div class="col-12">
             <div class="card" >
-                <div class="card-body pt-4 pb-2 px-4" style="min-height:69.2px">
-                    <h5 style="position:absolute;top: 25px;">Laporan Guru Matpel</h5>
-                    <button id="btn-filter" style="float:right;width:110px" class="btn btn-light ml-2 hidden" type="button"><i class="simple-icon-equalizer mr-2" style="transform-style: ;" ></i>Filter</button>
-                    <div class="dropdown float-right">
-                        <button id="btn-export" type="button" class="btn btn-outline-primary dropdown-toggle float-right hidden"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="simple-icon-share-alt mr-1"></i> Export
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="btn-export" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 37px, 0px);">
-                            <a class="dropdown-item" href="#" id="sai-rpt-print"><img src="{{ asset('img/Print.svg') }}" style="width:16px;"> <span class="ml-2">Print</span></a>
-                            <a class="dropdown-item" href="#" id="sai-rpt-print-prev"><img src="{{ asset('img/PrintPreview.svg') }}" style="width:16px;height: 16px;"> <span class="ml-2">Print Preview</span></a>
-                            <a class="dropdown-item" href="#" id="sai-rpt-excel"><img src="{{ asset('img/excel.svg') }}" style="width:16px;"> <span class="ml-2">Excel</span></a>
-                            <a class="dropdown-item" href="#" id="sai-rpt-email"><img src="{{ asset('img/email.svg') }}" style="width:16px;height: 16px;margin-right: 3px;"><span class="ml-2">Email</span></a>
-                        </div>
-                    </div>
-                </div>
+                <x-report-header judul="Laporan Guru Matpel"/>
                 <div class="separator"></div>
                 <div class="row">
                     <div class="col-12 col-sm-12">
@@ -40,62 +25,12 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-12 col-sm-12">
-                        <div class="collapse" id="collapsePaging">
-                            <div class="px-4 py-0 row"  style="min-height:63px">
-                                <label class="col-sm-1 pr-0" style="padding-top: 0;margin:auto">Menampilkan</label>
-                                <div class='col-sm-2 pl-0' style='padding-top: 0;margin:auto'>
-                                    <select name="show" id="show" class="" style='border:none'>
-                                        <option value="10">10 per halaman</option>
-                                        <option value="25">25 per halaman</option>
-                                        <option value="50">50 per halaman</option>
-                                        <option value="100">100 per halaman</option>
-                                        <option value="All" selected>Semua halaman</option>
-                                    </select>
-                                </div>
-                                <div class="col-sm-9 text-center">
-                                    <div id="pager">
-                                        <ul id="pagination" class="pagination pagination-sm2 float-right mb-0"></ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <x-report-paging/>
                 </div>                    
             </div>
         </div>
     </div>
-    <div class="row mt-2 hidden scroll" id="saku-report">
-        <div class="col-12 pr-0">
-            <div class="card px-0 py-4" style="min-height:200px">
-                <div class="border-bottom px-0 py-3 mb-2 navigation-lap hidden">
-                    <nav class="breadcrumb-container d-none d-sm-block d-lg-inline-block" aria-label="breadcrumb">
-                        <ol class="breadcrumb py-0 my-0">
-                            <li class="breadcrumb-item active">
-                                Laporan Penilaian
-                            </li>
-                        </ol>
-                    </nav>            
-                    <button type="button" id="btn-back" style="position: absolute;right: 25px;
-                    top: 30px;" class="btn btn-light float-right">
-                    <i class=""></i> Back</button>
-                </div>
-                <div class="row h-100" id="report-load" style="display: none;">
-                    <div class="col-12 col-md-10 mx-auto my-auto">
-                        <div style="box-shadow:none" class="card auth-card text-center">
-                            <div style="padding:50px;width:50%;" class="my-auto mx-auto">
-                                <div class="progress" style="height:10px">
-                                    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;" id="report-load-bar">0.00%</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div id="canvasPreview">
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-report-result judul="Guru Matpel" padding="px-0 py-4"/>
 
     @include('modal_search')
     @include('modal_email')
@@ -257,7 +192,6 @@
                 'nik_guru[1]' : $nik_guru.from,
                 'nik_guru[2]' : $nik_guru.to
             },{
-                ,{
                 'kode_pp[0]' : $kode_pp.type,
                 'kode_pp[1]' : $kode_pp.from,
                 'kode_pp[2]' : $kode_pp.to,
@@ -270,19 +204,17 @@
                 'kode_kelas[0]' : $kode_kelas.type,
                 'kode_kelas[1]' : $kode_kelas.from,
                 'kode_kelas[2]' : $kode_kelas.to
-            },
             }],
             orderby:[[],[],[],[],[]],
             width:[['30%','70%'],['30%','70%'],['30%','70%'],['30%','70%'],['30%','70%']],
             display:['kode','kode','kode','kode','kode']
-            
         });
 
         $('#inputFilter').on('change','input',function(e){
             setTimeout(() => {
                 $('#inputFilter').reportFilter({
-                    kode : ['kode_pp','kode_ta','kode_matpel','kode_kelas','kode_sem'],
-                    nama : ['PP','Tahun Ajaran','Mata Pelajaran','Kelas','Semester'],
+                    kode : ['kode_pp','kode_ta','nik_guru','kode_kelas','kode_matpel'],
+                    nama : ['PP','Tahun Ajaran','NIK Guru','Kelas','Mata Pelajaran'],
                     header : [['Kode', 'Nama'],['Kode','Nama'],['Kode','Nama'],['Kode','Nama'],['Kode','Nama']],
                     headerpilih : [['Kode', 'Nama','Action'],['Kode', 'Nama','Action'],['Kode', 'Nama','Action'],['Kode', 'Nama','Action'],['Kode', 'Nama','Action']],
                     columns: [
@@ -293,17 +225,17 @@
                             { data: 'kode_ta' },
                             { data: 'nama' }
                         ],[
-                            { data: 'kode_matpel' },
+                            { data: 'nik' },
                             { data: 'nama' }
                         ],[
                             { data: 'kode_kelas' },
                             { data: 'nama' }
                         ],[
-                            { data: 'kode_sem' },
+                            { data: 'kode_matpel' },
                             { data: 'nama' }
                         ]
                     ],
-                    url :["{{ url('sekolah-report/filter-pp') }}","{{ url('sekolah-report/filter-ta') }}","{{ url('sekolah-report/filter-matpel') }}","{{ url('sekolah-report/filter-kelas') }}","{{ url('sekolah-report/filter-semester') }}"],
+                    url :["{{ url('sekolah-report/filter-pp') }}","{{ url('sekolah-report/filter-ta') }}","{{ url('sekolah-report/filter-guru') }}","{{ url('sekolah-report/filter-kelas') }}","{{ url('sekolah-report/filter-matpel') }}"],
                     parameter:[{},{
                         'kode_pp[0]' : $kode_pp.type,
                         'kode_pp[1]' : $kode_pp.from,
@@ -314,10 +246,7 @@
                     },{
                         'kode_pp[0]' : $kode_pp.type,
                         'kode_pp[1]' : $kode_pp.from,
-                        'kode_pp[2]' : $kode_pp.to,
-                        'flag_aktif[0]' : '=',
-                        'flag_aktif[1]' : '1',
-                        'flag_aktif[2]' : ''
+                        'kode_pp[2]' : $kode_pp.to
                     },{
                         'kode_pp[0]' : $kode_pp.type,
                         'kode_pp[1]' : $kode_pp.from,
@@ -325,14 +254,26 @@
                         'flag_aktif[0]' : '=',
                         'flag_aktif[1]' : '1',
                         'flag_aktif[2]' : '',
-                        'kode_matpel[0]' : $kode_matpel.type,
-                        'kode_matpel[1]' : $kode_matpel.from,
-                        'kode_matpel[2]' : $kode_matpel.to
-                    },{}],
+                        'nik_guru[0]' : $nik_guru.type,
+                        'nik_guru[1]' : $nik_guru.from,
+                        'nik_guru[2]' : $nik_guru.to
+                    },{
+                        'kode_pp[0]' : $kode_pp.type,
+                        'kode_pp[1]' : $kode_pp.from,
+                        'kode_pp[2]' : $kode_pp.to,
+                        'flag_aktif[0]' : '=',
+                        'flag_aktif[1]' : '1',
+                        'flag_aktif[2]' : '',
+                        'nik_guru[0]' : $nik_guru.type,
+                        'nik_guru[1]' : $nik_guru.from,
+                        'nik_guru[2]' : $nik_guru.to,
+                        'kode_kelas[0]' : $kode_kelas.type,
+                        'kode_kelas[1]' : $kode_kelas.from,
+                        'kode_kelas[2]' : $kode_kelas.to
+                    }],
                     orderby:[[],[],[],[],[]],
                     width:[['30%','70%'],['30%','70%'],['30%','70%'],['30%','70%'],['30%','70%']],
                     display:['kode','kode','kode','kode','kode']
-                    
                 });
             }, 500);
         });
