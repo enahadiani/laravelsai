6cea7aad26d53c1e861d4e94195d0cc771f7688b
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2020-11-18 10:06:03 +0700

    Update

--- app/Http/Controllers/Webginas/BannerController.php
+++ app/Http/Controllers/Webginas/BannerController.php
@@ -12,20 +12,21 @@ class BannerController extends Controller {
     public function show(){
         try {
             $client = new Client();
-            $response = $client->request('GET',  config('api.url').'admginas-master/banner-web',[
-                'headers' => [
-                    'Authorization' => 'Bearer '.Session::get('token'),
-                    'Accept'     => 'application/json',
-                ]
-            ]);
+            var_dump(config('api.url'));
+            // $response = $client->request('GET',  config('api.url').'admginas-master/banner-web',[
+            //     'headers' => [
+            //         'Authorization' => 'Bearer '.Session::get('token'),
+            //         'Accept'     => 'application/json',
+            //     ]
+            // ]);
 
-            if ($response->getStatusCode() == 200) { // 200 OK
-                $response_data = $response->getBody()->getContents();
+            // if ($response->getStatusCode() == 200) { // 200 OK
+            //     $response_data = $response->getBody()->getContents();
                 
-                $data = json_decode($response_data,true);
-                $data = $data["data"];
-            }
-            return response()->json(['daftar' => $data, 'status'=>true], 200); 
+            //     $data = json_decode($response_data,true);
+            //     $data = $data["data"];
+            // }
+            // return response()->json(['daftar' => $data, 'status'=>true], 200); 
 
         } catch (BadResponseException $ex) {
             $response = $ex->getResponse();
