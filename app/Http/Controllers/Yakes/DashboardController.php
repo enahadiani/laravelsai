2cac6a5936df01c779193555eae2281e5a0c66c7
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2020-12-04 16:49:19 +0700

    Update

--- app/Http/Controllers/Yakes/DashboardController.php
+++ app/Http/Controllers/Yakes/DashboardController.php
@@ -15,6 +15,25 @@
             }
         }
 
+        public function getFilterTahun($periode) {
+            $periode = "202011";
+            $client = new Client();
+            $response = $client->request('GET',  config('api.url').'yakes-dash/getFilterTahunDash',[
+                'headers' => [
+                    'Authorization' => 'Bearer '.Session::get('token'),
+                    'Accept'     => 'application/json',
+                ]
+            ]);
+
+            if ($response->getStatusCode() == 200) { // 200 OK
+                $response_data = $response->getBody()->getContents();
+            
+                $data = json_decode($response_data,true);
+                $data = $data['data'];
+            }
+            return response()->json(['daftar' => $data, 'status' => true], 200);
+        }
+
         public function getdataEdu($periode) {
             $periode = "202011";
             $client = new Client();
