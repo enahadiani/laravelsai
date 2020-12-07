cdfaad4fdab594a7b57e06ffac2386faef8e0aa4
enahadiani <enahadiani2@gmail.com>
2020-12-07 14:20:15 +0700

    update fontsize blank blade

--- resources/views/yakes/blank.blade.php
+++ resources/views/yakes/blank.blade.php
@@ -3,7 +3,7 @@
         <div class="card auth-card" style="box-shadow:none">
             <div class="text-center" style="padding:80px;width:100%">
                 <img src="{{ asset('img/report_blank.svg') }}" style="width:200px">
-                <h2 class="mt-3"><b>Data tidak ditemukan</b></h2>
+                <h5 class="mt-3"><b>Data tidak ditemukan</b></h5>
                 <p class="mt-3" style="font-size:12px">Cek ulang filter yang sudah diterapkan.</p>
             </div>
         </div>
