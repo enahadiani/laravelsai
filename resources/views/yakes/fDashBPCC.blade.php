e602bce215a9046c1766f87e777bdcfd0f0ec905
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2020-12-10 16:02:10 +0700

    Update dashboard bpcc

--- resources/views/yakes/fDashBPCC.blade.php
+++ resources/views/yakes/fDashBPCC.blade.php
@@ -72,6 +72,7 @@
     .footer-dashboard {
         width: 100%;
         margin-bottom: 100px;
+        padding-bottom: 55px;
         height: 50px;
     }
     .dropdown-menu {
@@ -91,7 +92,29 @@
         background-color: #F5F5F5;
         cursor: pointer;
     }
+    .button-top {
+        display: none;
+        position: fixed;
+        bottom: 20px;
+        right: 30px;
+        z-index: 10;
+        border: none;
+        outline: none;
+        background-color: #d3d3d3;
+        cursor: pointer;
+        padding: 15px;
+        width: 50px;
+        border-radius: 50%;
+    }
+    .button-top:hover {
+        background-color: #c6c6c6;
+    }
 </style>
+
+<button id="button-top" class="button-top" onclick="topFunction()">
+        <span class="simple-icon-arrow-up"></span>
+</button>
+
 <div id="filter-header">
     <div class="row">
         <div class="col-12">
@@ -101,8 +124,8 @@
     <div class="row">
         <div class="col-2">
             <div class="dropdown-periode dropdown">
-                <button class="btn btn-light select-dash" style="background-color: #ffffff;width: 160px;text-align:left;" type="button" data-toggle="dropdown">
-                    {{Session::get('periode')}}
+                <button class="btn btn-light select-dash" style="background-color: #ffffff;width: 180px;text-align:left;" type="button" data-toggle="dropdown">
+                    Periode : {{Session::get('periode')}}
                     <span class="glyph-icon simple-icon-arrow-down" style="float: right; margin-top:3%;"></span>
                 </button>
                 <ul class="dropdown-menu periode" role="menu" aria-labelledby="menu1">
@@ -244,14 +267,22 @@ var periode = "{{Session::get('periode')}}";
 var pembagi = 1000000;
 
 var header = document.getElementById('filter-header');
+var buttonTop = document.getElementById('button-top');
 var sticky = header.offsetTop;
 window.onscroll = function() {
     if(window.pageYOffset > sticky) {
         header.classList.add('fixed-filter')
+        buttonTop.style.display = 'block';
     } else {
         header.classList.remove('fixed-filter')
+        buttonTop.style.display = 'none';
     }
 }
+
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
 
     $.ajax({
         type:'GET',
@@ -266,7 +297,7 @@ window.onscroll = function() {
 
     $('.periode').on( 'click', 'li', function() {
         var text = $(this).html();
-        var htmlText = text+"<span class='glyph-icon simple-icon-arrow-down' style='float: right; margin-top:3%;'></span>";
+        var htmlText = "Periode : "+text+"<span class='glyph-icon simple-icon-arrow-down' style='float: right; margin-top:3%;'></span>";
         $(this).closest('.dropdown-periode').find('.select-dash').html(htmlText);
         periode = text;
         $('#real-bp').empty();
