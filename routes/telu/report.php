ffb986b20781fee77601797f0f77eb277c878a27
enahadiani <enahadiani2@gmail.com>
2020-12-22 16:28:51 +0700

    update dash telu

--- routes/telu/report.php
+++ routes/telu/report.php
@@ -17,3 +17,4 @@ Route::get('filter-output', 'DashTelu\FilterController@getFilterOutput');
 
 Route::post('lap-labarugi-agg', 'DashTelu\LaporanController@getLabaRugiAgg');
 Route::post('lap-neraca2', 'DashTelu\LaporanController@getNeraca2');
+Route::post('lap-investasi', 'DashTelu\LaporanController@getInvestasi');
