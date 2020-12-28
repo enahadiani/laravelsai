258a7326610ba250d19159d6227205b6f9e42d6a
enahadiani <enahadiani2@gmail.com>
2020-12-28 08:45:36 +0700

    update dash telu

--- resources/views/dash-telu/fDashMSPendapatan.blade.php
+++ resources/views/dash-telu/fDashMSPendapatan.blade.php
@@ -75,6 +75,12 @@ $thnLalu = substr($tahunLalu,2,2)
         color:white;
         background:red;
     }
+
+    .preview-close > span
+    {
+        color:white;
+        background:red;
+    }
     </style>
 
 <div class="container-fluid mt-3">
@@ -143,6 +149,7 @@ $thnLalu = substr($tahunLalu,2,2)
         <div class="modal-dialog modal-dialog-centered" role="document" style="max-width:1000px">
             <div class="modal-content">
                 <div class="modal-header pb-0" style="border:none">
+                    <h6 class="card-title mb-0">Pencapaian Pendapatan Terhadap RKA Bulan Berjalan</h6>
                     <button type="button" class="close preview-close" data-dismiss="modal" aria-label="Close">
                          <span aria-hidden="true">&times;</span>
                     </button>
@@ -159,6 +166,7 @@ $thnLalu = substr($tahunLalu,2,2)
         <div class="modal-dialog modal-dialog-centered" role="document" style="max-width:1000px">
             <div class="modal-content">
                 <div class="modal-header pb-0" style="border:none">
+                    <h6 class="card-title mb-0">Capaian Kelompok Pdpt Non Pdkk</h6>
                     <button type="button" class="close preview-close" data-dismiss="modal" aria-label="Close">
                          <span aria-hidden="true">&times;</span>
                     </button>
