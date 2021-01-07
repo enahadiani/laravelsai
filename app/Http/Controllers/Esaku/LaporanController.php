479eaadf67c61be70d6d88459efd45e023e3c10b
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-01-07 16:02:39 +0700

    Update

--- app/Http/Controllers/Esaku/LaporanController.php
+++ app/Http/Controllers/Esaku/LaporanController.php
@@ -291,7 +291,7 @@
 
         public function getPenjualan(Request $request) {
            try{
-                $client = new Client();
+                $client = new Client(););
                 $response = $client->request('GET',  config('api.url').'toko-report/lap-penjualan',[
                     'headers' => [
                         'Authorization' => 'Bearer '.Session::get('token'),
@@ -308,7 +308,7 @@
                     $response_data = $response->getBody()->getContents();
                     
                     $res = json_decode($response_data,true);
-                    $data = $res;
+                    $data = $res["data"];
                 }
                 if($request->periode != ""){
                     $periode = $request->periode;
