095b340782ab69f1a05e7dbe01272e50df2003cb
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-01-08 14:59:57 +0700

    Update laporan penjualan harian

--- resources/views/esaku/fLapPnjHari.blade.php
+++ resources/views/esaku/fLapPnjHari.blade.php
@@ -2,7 +2,7 @@
 <div class="row" id="saku-filter">
     <div class="col-12">
         <div class="card" >
-            <x-report-header judul="Laporan Penjualan"/>
+            <x-report-header judul="Laporan Penjualan Harian"/>
             <div class="separator"></div>
                 <div class="row">
                     <div class="col-12 col-sm-12">
@@ -12,7 +12,7 @@
                                     <h6>Filter</h6>
                                     <div id="inputFilter">
                                         <!-- COMPONENT -->
-                                        <x-inp-filter kode="periode" nama="Periode" selected="3" :option="array('3')"/>
+                                        <x-inp-filter kode="periode" nama="Tanggal" selected="3" :option="array('3')"/>
                                         <x-inp-filter kode="kasir" nama="Kasir" selected="1" :option="array('1','3')"/>
                                         <x-inp-filter kode="no_bukti" nama="No Bukti" selected="1" :option="array('1','2','3','i')"/>
                                         <!-- END COMPONENT -->
@@ -28,7 +28,7 @@
         </div>
     </div>
 </div>
-<x-report-result judul="Laporan Penjualan" padding="px-4 py-4"/>
+<x-report-result judul="Laporan Penjualan Harian" padding="px-4 py-4"/>
 
 @include('modal_search')
 @include('modal_email')
@@ -61,7 +61,7 @@
             to : "",
             toname : "",
         }
-    var $no_bukti = {
+    var $tanggal = {
             type : "all",
             from : "",
             fromname : "",
@@ -106,10 +106,10 @@
 
     $('.selectize').selectize();
     $('#inputFilter').reportFilter({
-        kode : ['periode','kasir','no_bukti'],
-        nama : ['Periode','Kasir','No Bukti'],
-        header : [['Periode'],['Kode','Nama'],['No Bukti','Keterangan']],
-        headerpilih : [['Periode','Action'],['Kode','Nama','Action'],['No Bukti','Keterangan','Action']],
+        kode : ['periode','kasir','tanggal'],
+        nama : ['Tanggal','Kasir','No Bukti'],
+        header : [['Periode'],['Kode','Nama'],['Tanggal']],
+        headerpilih : [['Periode','Action'],['Kode','Nama','Action'],['Tanggal','Action']],
         columns: [
             [
                 { data: 'periode' },
@@ -117,24 +117,23 @@
                 { data: 'nik_user' },
                 { data: 'nama' }
             ],[
-                { data: 'no_jual' },
-                { data: 'keterangan' }
+                { data: 'tanggal' }
             ]
         ],
-        url :["{{ url('esaku-report/filter-periode') }}","{{ url('esaku-report/filter-nik') }}","{{ url('esaku-report/filter-bukti') }}"],
+        url :["{{ url('esaku-report/filter-periode') }}","{{ url('esaku-report/filter-nik') }}","{{ url('esaku-report/filter-tanggal') }}"],
         parameter:[{},{},{}],
         orderby:[[[0,"desc"]],[[0,"desc"]],[[0,"asc"]]],
         width:[['30%','70%'],['30%','70%'],['30%','70%'],['30%','70%']],
         display:['kode','kode','kode'],
         pageLength:[12,10,10]
-    })
+    });
     $('#inputFilter').on('change','input',function(e){
         setTimeout(() => {
             $('#inputFilter').reportFilter({
-                kode : ['periode','kasir','no_bukti'],
-                nama : ['Periode','Kasir','No Bukti'],
-                header : [['Periode'],['Kode','Nama'],['No Bukti','Keterangan']],
-                headerpilih : [['Periode','Action'],['Kode','Nama','Action'],['No Bukti','Keterangan','Action']],
+                kode : ['periode','kasir','tanggal'],
+                nama : ['Tanggal','Kasir','No Bukti'],
+                header : [['Periode'],['Kode','Nama'],['Tanggal']],
+                headerpilih : [['Periode','Action'],['Kode','Nama','Action'],['Tanggal','Action']],
                 columns: [
                     [
                         { data: 'periode' },
@@ -142,17 +141,16 @@
                         { data: 'nik_user' },
                         { data: 'nama' }
                     ],[
-                        { data: 'no_jual' },
-                        { data: 'keterangan' }
+                        { data: 'tanggal' }
                     ]
                 ],
-                url :["{{ url('esaku-report/filter-periode') }}","{{ url('esaku-report/filter-nik') }}","{{ url('esaku-report/filter-bukti') }}"],
+                url :["{{ url('esaku-report/filter-periode') }}","{{ url('esaku-report/filter-nik') }}","{{ url('esaku-report/filter-tanggal') }}"],
                 parameter:[{},{},{}],
                 orderby:[[[0,"desc"]],[[0,"desc"]],[[0,"asc"]]],
                 width:[['30%','70%'],['30%','70%'],['30%','70%'],['30%','70%']],
                 display:['kode','kode','kode'],
                 pageLength:[12,10,10]
-            })
+            });
         }, 500)
     });
 
@@ -166,22 +164,22 @@
         $formData.append("nik_kasir[]",$kasir.type);
         $formData.append("nik_kasir[]",$kasir.from);
         $formData.append("nik_kasir[]",$kasir.to);
-        $formData.append("no_bukti[]",$no_bukti.type);
-        $formData.append("no_bukti[]",$no_bukti.from);
-        $formData.append("no_bukti[]",$no_bukti.to);
+        $formData.append("tanggal[]",$tanggal.type);
+        $formData.append("tanggal[]",$tanggal.from);
+        $formData.append("tanggal[]",$tanggal.to);
         for(var pair of $formData.entries()) {
             console.log(pair[0]+ ', '+ pair[1]); 
         }
         $('#saku-report').removeClass('hidden');
-        xurl = "{{ url('esaku-auth/form/rptPnj3') }}";
+        xurl = "{{ url('esaku-auth/form/rptPnjHari') }}";
         $('#saku-report #canvasPreview').load(xurl);
     });
 
     $('#show').change(function(e){
         $formData = new FormData();
-        $formData.append("periode[]",$periode.type);
-        $formData.append("periode[]",$periode.from);
-        $formData.append("periode[]",$periode.to);
+        $formData.append("periode[]",$tanggal.type);
+        $formData.append("periode[]",$tanggal.from);
+        $formData.append("periode[]",$tanggal.to);
         $formData.append("nik_kasir[]",$kasir.type);
         $formData.append("nik_kasir[]",$kasir.from);
         $formData.append("nik_kasir[]",$kasir.to);
@@ -192,7 +190,7 @@
             console.log(pair[0]+ ', '+ pair[1]); 
         }
         $('#saku-report').removeClass('hidden');
-        xurl = "{{ url('esaku-auth/form/rptPnj3') }}";
+        xurl = "{{ url('esaku-auth/form/rptPnjHari') }}";
         $('#saku-report #canvasPreview').load(xurl);
     });
 
