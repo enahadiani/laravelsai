ed839af9ce159f6842482ecd6dfa92f0d3fa4f67
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-01-29 16:01:40 +0700

    Create web esaku home

--- resources/views/webesaku/home.blade.php
+++ resources/views/webesaku/home.blade.php
@@ -1,1 +1,96 @@
-Test
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="PT. Samudera Aplikasi Indonesia" />
+    <meta name="description" content="Penyedia software keuangan">
+    <link rel="icon" type="image/png" href="{{ asset('asset_web/img/SAKU2021.svg') }}">   
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Document title -->
+    <title>SAKU | Home</title>
+    <!-- Stylesheets & Fonts -->
+    <link href="{{ asset('asset_web/css/plugins.css') }}" rel="stylesheet">
+    <link href="{{ asset('asset_web/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('asset_web/css/websaku.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    {{-- Body Inner --}}
+    <div class="body-inner">
+
+        {{-- Header --}}
+        <header id="header" data-transparent="true" data-fullwidth="true" class="submenu-light">
+            <div class="header-inner">
+                <div class="container header-content">
+                    <div class="header-content-1">
+                        <div id="logo">
+                            <a href="index.html">
+                                <span class="logo-default">
+                                    <img src=" {{ asset('asset_web/img/SAKU2021.svg') }}" alt="logo" width="80"/>
+                                </span>
+                                <span class="logo-dark">
+                                    <img src=" {{ asset('asset_web/img/SAKU2021.svg') }}" alt="logo" width="80"/>
+                                </span>
+                            </a>
+                        </div>
+                        <div id="mainMenu-trigger">
+                            <a class="lines-button x"><span class="lines"></span></a>
+                        </div>
+                        <div id="mainMenu" class="main-menu">
+                            <div class="container">
+                                <nav>
+                                    <ul>
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">Produk</a></li>
+                                        <li><a href="#">Perusahaan</a></li>
+                                        <li><a href="#">Harga</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="header-content-2">
+                        <div id="mainMenu2" class="main-menu">
+                            <div class="container">
+                                <a href="#" id="masuk-link">Masuk</a>
+                                <a href="#" id="daftar-link">Daftar Gratis</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        {{-- end: Header --}}
+
+        {{-- Section Welcome --}}
+        <section id="welcome" class="p-b-0">
+            <div class="container">
+                <div class="heading-text heading-section text-center m-b-40" data-animate="fadeInUp">
+                    <h1>Software Pencatatan Bisnis</h1>
+                    <h5 class="lead">
+                        Mengelola pencatatan bisnis anda sehingga menghasilkan laporan handal dan
+                        dashboard bermanfaat.
+                    </h5>
+                </div>
+                <div class="action-welcome">
+                    <button type="button" id="daftar-welcome">Daftar Gratis</button>
+                    <button type="button" id="hubungi-welcome">Hubungi Kami</button>
+                </div>
+            </div>
+        </section>
+
+    </div>
+    {{-- end: Body Inner --}}
+    {{-- Scroll top --}}
+    <a id="scrollTop"><i class="icon-chevron-up"></i><i class="icon-chevron-up"></i></a>
+    {{-- end: Scroll top --}}
+
+    {{-- Plugins --}}
+    <script src="{{ asset('asset_web/js/jquery.js')}}"></script>
+    <script src="{{ asset('asset_web/js/plugins.js')}}"></script>
+    <script src="{{ asset('asset_web/js/functions.js')}}"></script>
+</body>
+
+</html>
