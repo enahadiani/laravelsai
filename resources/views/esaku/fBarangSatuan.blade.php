d8b24f82440aa0659e5183eb34a80d7c76df6ea2
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-02-08 13:08:32 +0700

    Update form satuan barang

--- resources/views/esaku/fBarangSatuan.blade.php
+++ resources/views/esaku/fBarangSatuan.blade.php
@@ -1,4 +1,5 @@
     <link rel="stylesheet" href="{{ asset('master.css') }}" />
+    <link rel="stylesheet" href="{{ asset('form.css') }}" />
     <!-- LIST DATA -->
     <x-list-data judul="Data Barang Satuan" tambah="true" :thead="array('Kode','Nama','Tgl Input','Aksi')" :thwidth="array(20,70,0,10)" :thclass="array('','','','text-center')" />
     <!-- END LIST DATA -->
@@ -8,10 +9,11 @@
         <div class="row" id="saku-form" style="display:none;">
             <div class="col-12">
                 <div class="card">
-                    <div class="card-body form-header" style="padding-top:1rem;padding-bottom:1rem;">
-                        <h6 id="judul-form" style="position:absolute;top:25px"></h6>
-                        <button type="submit" class="btn btn-primary ml-2"  style="float:right;" id="btn-save"><i class="fa fa-save"></i> Simpan</button>
-                        <button type="button" class="btn btn-light ml-2" id="btn-kembali" style="float:right;"><i class="fa fa-undo"></i> Keluar</button>
+                    <div class="card-body form-header" style="padding-top:0.5rem;padding-bottom:0.5rem;min-height:48px;">
+                        <h6 id="judul-form" style="position:absolute;top:13px"></h6>
+                        <button type="button" id="btn-kembali" aria-label="Kembali" class="btn btn-back">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
                     <div class="separator mb-2"></div>
                     <!-- FORM BODY -->
@@ -25,16 +27,23 @@
                         </div>
                         <div class="form-row">
                             <div class="form-group col-md-6 col-sm-12">
-                                <div class="row">
-                                    <div class="col-md-6 col-sm-12">
-                                        <label for="kode_satuan" >Kode</label>
-                                        <input class="form-control" type="text" name="kode_satuan" id="kode_satuan" required>
-                                    </div>
-                                    <div class="col-md-6 col-sm-12">
-                                        <label for="nama">Nama</label>
-                                        <input class="form-control" type="text" id="nama" name="nama" required>
-                                    </div>
-                                </div>
+                                <label for="kode_satuan">Kode</label>
+                                <input class="form-control" type="text" name="kode_satuan" id="kode_satuan" required>
+                            </div>
+                            <div class="form-group col-md-6 col-sm-12">
+                               <label for="nama">Nama</label>
+                                <input class="form-control" type="text" id="nama" name="nama" required>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- Save Button --}}
+                    <div class="card-form-footer">
+                        <div class="footer-form-container">
+                            <div class="text-right message-action">
+                                <p class="text-success"></p>
+                            </div>
+                            <div class="action-footer">
+                                <button type="submit" style="margin-top: 10px;" class="btn btn-primary btn-save"><i class="fa fa-save"></i> Simpan</button>
                             </div>
                         </div>
                     </div>
@@ -49,7 +58,9 @@
     <script src="{{ asset('helper.js') }}"></script>
     <script>
     setHeightForm();
-    
+    // Small Form
+    $('#saku-form > .col-12').addClass('mx-auto col-lg-6');
+    $('#modal-preview > .modal-dialog').css({ 'max-width':'600px'});
     $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
