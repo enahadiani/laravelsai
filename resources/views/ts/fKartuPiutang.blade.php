0498f6b9e01011f79f7400961c11d8059bd69b07
enahadiani <enahadiani2@gmail.com>
2021-02-10 11:45:55 +0700

    update dash ts

--- resources/views/ts/fKartuPiutang.blade.php
+++ resources/views/ts/fKartuPiutang.blade.php
@@ -252,7 +252,7 @@
             width: 100%;
             height: 1px;
             /* dashed border */
-            background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='%23333' stroke-width='4' stroke-dasharray='1%2c 12' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
+            background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='%23B0AFB0FF' stroke-width='4' stroke-dasharray='1%2c 12' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
         }
     </style>
     <div id='grid-load'><img src='{{ asset("img/loadgif.gif") }}' style='width:25px;height:25px'></div>
