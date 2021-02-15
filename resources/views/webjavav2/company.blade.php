3ad788a476efc00a23d30cbc0a58048480c54ab8
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-02-15 14:34:30 +0700

    Update company page web java

--- resources/views/webjavav2/company.blade.php
+++ resources/views/webjavav2/company.blade.php
@@ -12,8 +12,8 @@
     <!-- Stylesheets & Fonts -->
     <link href="{{ asset('asset_web/css/plugins.css') }}" rel="stylesheet">
     <link href="{{ asset('asset_web/css/style.css') }}" rel="stylesheet">
-    <link href="{{ asset('asset_web/css/webjava/contact-dekstop.css') }}" rel="stylesheet">
-    <link href="{{ asset('asset_web/css/webjava/contact-mobile.css') }}" rel="stylesheet">
+    <link href="{{ asset('asset_web/css/webjava/company-dekstop.css') }}" rel="stylesheet">
+    <link href="{{ asset('asset_web/css/webjava/company-mobile.css') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -67,8 +67,64 @@
             <div class="bg-overlay"></div>
             <div class="container">
                 <div class="page-title">
-                    <h1 class="text-uppercase text-medium">Contact</h1>
+                    <h1 class="text-uppercase text-medium">Company</h1>
                 </div>
+            </div>
+        </section>
+
+        {{-- Section 2 --}}
+        <section class="p-t-100 p-b-100 section-fullwidth">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 p-l-70 p-r-40">
+                    <div class="m-b-40">
+                        <h2>Company Profile</h2>
+                        <p>
+                            PT JAVA PRATAMA ENERGI established on October 3rd, 2014, by Notary Etic Srimartini, SH. MKn. 
+                            The company domiciled in Jl. Maskumambang No. 9, Turangga, Lengkong, Bandung.
+                        </p>
+                        <p>
+                            Founders of PT JAVA PRATAMA ENERGI background are practitioners in renewable energy, 
+                            as engineer, turbine manufacturer, and power plant manager that have the same vision to 
+                            create competitive local manufacturer of rotating turbomachinery both in hydro & thermal 
+                            energy.
+                        </p>
+                        <p>
+                            Equipped with experienced workmanship and production facility, We assure to deliver 
+                            highest quality of product to meet customer satisfaction. Sustain and reliable is our motto. 
+                            We strives upholding business transparency and considering longterm relations with all 
+                            stakeholder, i.e. clients, employees, subcontractors, suppliers and services provider.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 m-b-30 col-dekstop">
+                    <img alt="logo-company" src="{{ asset('asset_web/webjava/images/company.png') }}" width="600"/>
+                </div>
+            </div>
+        </section>
+
+        {{-- Section 3 --}}
+        <section class="box-fancy section-fullwidth text-light no-padding">
+            <div class="row company-profile-box">
+                <div class="col-lg-6 text-left" style="background-color: #012068">
+                    <h2>VISION</h2>
+                    <span>Being a leading EPC company by always maintain consistency of quality product sustainability. </span>
+                </div>
+
+                <div class="col-lg-6 text-left" style="background-color: #002D96">
+                    <h2>MISSION</h2>
+                    <p>Participate in global renewable energy development.</p>
+                    <p>Provide generating equipment for small and medium scale for hydro and thermal powerplant. </p>
+                </div>
+            </div>
+        </section>
+
+        {{-- Section 4 --}}
+        <section class="p-t-100 p-b-100">
+            <div class="container">
+                <div class="heading-text text-center heading-section m-b-80">
+                    <h2>Organization</h2>
+                </div>
+                <div id="organization"></div>
             </div>
         </section>
 
@@ -200,8 +256,176 @@
     <!--Plugins-->
     <script src="{{ asset('asset_web/js/jquery.js') }}"></script>
     <script src="{{ asset('asset_web/js/plugins.js') }}"></script>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/sankey.js"></script>
+    <script src="https://code.highcharts.com/modules/organization.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+
 
     <!--Template functions-->
     <script src="{{ asset('asset_web/js/functions.js')}}"></script>
+    <script type="text/javascript">
+        Highcharts.chart('organization', {
+            chart: {
+                height: 900,
+                inverted: true
+            },
+            title: {
+                text: ''
+            },
+            accessibility: {
+                point: {
+                    descriptionFormatter: function (point) {
+                        var nodeName = point.toNode.name,
+                            nodeId = point.toNode.id,
+                            nodeDesc = nodeName === nodeId ? nodeName : nodeName + ', ' + nodeId,
+                            parentDesc = point.fromNode.id;
+                        return point.index + '. ' + nodeDesc + ', reports to ' + parentDesc + '.';
+                    }
+                }
+            },
+            series: [{
+                type: 'organization',
+                name: 'Java Pratama Energi',
+                keys: ['from', 'to'],
+                data: [
+                    ['Java Pratama Energi', 'Commercial and Support'],
+                    ['Java Pratama Energi', 'Manufacturing and Support'],
+                    ['Java Pratama Energi', 'Engineering, HSE, dan QC/QA'],
+                    ['Commercial and Support', 'HRGA'],
+                    ['Commercial and Support', 'Finance/Support'],
+                    ['Commercial and Support', 'Procurement'],
+                    ['Manufacturing and Support', 'Construction'],
+                    ['Manufacturing and Support', 'Workshop'],
+                    ['Manufacturing and Support', 'Technical'],
+                    ['Engineering, HSE, dan QC/QA', 'Proccess and Design'],
+                    ['Engineering, HSE, dan QC/QA', 'HSE'],
+                    ['Engineering, HSE, dan QC/QA', 'QC/QA'],
+                    // ['CEO', 'CPO'],
+                    // ['CEO', 'CSO'],
+                    // ['CEO', 'HR'],
+                    // ['CTO', 'Product'],
+                    // ['CTO', 'Web'],
+                    // ['CSO', 'Sales'],
+                    // ['HR', 'Market'],
+                    // ['CSO', 'Market'],
+                    // ['HR', 'Market'],
+                    // ['CTO', 'Market']
+                ],
+                levels: [{
+                    level: 0,
+                    color: 'silver',
+                    dataLabels: {
+                        style: {
+                            fontSize: '8px'
+                        }
+                    },
+                    height: 25
+                }, {
+                    level: 1,
+                    color: '#007ad0',
+                    dataLabels: {
+                        style: {
+                            fontSize: '8px',
+                        }
+                    },
+                    height: 25,
+                }, {
+                    level: 2,
+                    color: '#980104',
+                    dataLabels: {
+                        style: {
+                            fontSize: '8px',
+                        }
+                    },
+                }, {
+                    level: 4,
+                    color: '#980104'
+                }],
+                nodes: [{
+                    id: 'Java Pratama Energi',
+                    column: 0,
+                },{
+                    id: 'Commercial and Support',
+                    column: 1,
+                    offset: '-30%'
+                    // title: 'CEO',
+                    // name: 'Grethe Hjetland',
+                    // image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131126/Highsoft_03862_.jpg'
+                }, {
+                    id: 'Manufacturing and Support',
+                    column: 1,
+                    offset: '0%'
+                    // title: 'HR/CFO',
+                    // name: 'Anne Jorunn Fjærestad',
+                    // color: '#007ad0',
+                    // image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131210/Highsoft_04045_.jpg'
+                }, {
+                    id: 'Engineering, HSE, dan QC/QA',
+                    column: 1,
+                    offset: '30%'
+                    // title: 'CTO',
+                    // name: 'Christer Vasseng',
+                    // image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131120/Highsoft_04074_.jpg'
+                }, {
+                    id: 'Proccess and Design',
+                    column: 2,
+                    // title: 'CPO',
+                    // name: 'Torstein Hønsi',
+                    // image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131213/Highsoft_03998_.jpg'
+                }, {
+                    id: 'QC/QA',
+                    column: 2,
+                    // title: 'CSO',
+                    // name: 'Anita Nesse',
+                    // image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131156/Highsoft_03834_.jpg'
+                }, {
+                    id: 'Construction',
+                    column: 2,
+                    // name: 'Product developers'
+                }, {
+                    id: 'Workshop',
+                    column: 2,
+                    // name: 'Web devs, sys admin'
+                }, {
+                    id: 'Technical',
+                    column: 2,
+                    // name: 'Sales team'
+                }, {
+                    id: 'HRGA',
+                    column: 2,
+                    // name: 'Sales team'
+                }, {
+                    id: 'Finance/Accounting',
+                    column: 2,
+                    // name: 'Sales team'
+                }, {
+                    id: 'Technical',
+                    column: 2,
+                    // name: 'Sales team'
+                },
+                // {
+                //     id: 'Market',
+                //     name: 'Marketing team',
+                //     column: 5
+                // }
+                ],
+                colorByPoint: false,
+                borderColor: 'white',
+                nodeWidth: 65
+            }],
+            tooltip: {
+                outside: true
+            },
+            exporting: {
+                enabled: false
+            },
+            credits: {
+                enabled: false
+            }
+
+        });
+    </script>
 </body>
 </html>
