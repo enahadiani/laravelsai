907c42427ecab53ddc86af318a0e259deb09bfe4
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-02-19 13:13:45 +0700

    Update header webjava

---
+++ resources/views/webjavav2/header.blade.php
@@ -0,0 +1,41 @@
+<header id="header" data-transparent="true" data-fullwidth="true" class="dark submenu-light ">
+            <div class="header-inner">
+                <div class="container">
+                    <div id="mainMenu-trigger">
+                        <a class="lines-button x"><span class="lines"></span></a>
+                    </div>
+                    <div class="header-container">   
+                        <div class="logo-nav">
+                            <div id="logo">
+                                <a href="https://app.simkug.com/webjava-v2/home">
+                                    <span class="logo-default">
+                                        <img alt="logo" width="70" src="{{ asset('asset_web/webjava/images/icon-java-blue.png') }}" />
+                                    </span>
+                                    <span class="logo-dark">
+                                        <img alt="logo" src="{{ asset('asset_web/webjava/images/icon-java-white.png') }}" />
+                                    </span>
+                                </a> 
+                            </div>
+                            <div id="mainMenu">
+                                <div class="container">
+                                    <nav>
+                                        <ul>
+                                            <li><a href="https://app.simkug.com/webjava-v2/home">Home</a></li>
+                                            <li><a href="https://app.simkug.com/webjava-v2/company">Company</a></li>
+                                            <li><a href="https://app.simkug.com/webjava-v2/product">Product</a></li>
+                                            <li><a href="https://app.simkug.com/webjava-v2/contact">Contact</a></li>
+                                        </ul>
+                                    </nav>
+                                    <div class="information-contact col-mobile">
+                                        <div>
+                                            <p>Phone: +62 22 87306036</p>
+                                            <p>Email: marketing@javaturbine.co.id</p>
+                                        </div>
+                                    </div>   
+                                </div>
+                            </div>
+                        </div>   
+                    </div>
+                </div>
+            </div>
+        </header>
