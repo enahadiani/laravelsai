a15d2770bd71ce032ef6228447ca1673ee3aee3f
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-02-22 16:48:28 +0700

    Create vendor form

--- routes/java/master.php
+++ routes/java/master.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Session;
 
 // Helper
 Route::get('customer-akun', 'Java\HelperController@getAkunCustomer');
+Route::get('vendor-akun', 'Java\HelperController@getAkunVendor');
 
 // Data Customer //
 Route::get('customer', 'Java\CustomerController@index');
@@ -12,5 +13,12 @@ Route::get('customer-show', 'Java\CustomerController@getData');
 Route::post('customer', 'Java\CustomerController@store');
 Route::put('customer-ubah', 'Java\CustomerController@update');
 Route::delete('customer', 'Java\CustomerController@delete');
+
+// Data Vendor //
+Route::get('vendor', 'Java\CustomerController@index');
+Route::get('vendor-show', 'Java\CustomerController@getData');
+Route::post('vendor', 'Java\CustomerController@store');
+Route::put('vendor-ubah', 'Java\CustomerController@update');
+Route::delete('vendor', 'Java\CustomerController@delete');
 
 ?>
