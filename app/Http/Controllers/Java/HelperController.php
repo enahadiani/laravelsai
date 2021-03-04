738ef53d7e2f3de42fb788c4d3c07402feaad059
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-03-04 15:38:17 +0700

    Create form laporan kartu proyek

--- app/Http/Controllers/Java/HelperController.php
+++ app/Http/Controllers/Java/HelperController.php
@@ -15,6 +15,24 @@ class HelperController extends Controller {
         }
     }
 
+    public function getKartuBukti() {
+        $client = new Client();
+        $response = $client->request('GET',  config('api.url').'java-report/filter-proyek',[
+            'headers' => [
+                'Authorization' => 'Bearer '.Session::get('token'),
+                'Accept'     => 'application/json',
+            ]
+        ]);
+
+        if ($response->getStatusCode() == 200) { // 200 OK
+            $response_data = $response->getBody()->getContents();
+            
+            $data = json_decode($response_data,true);
+            $data = $data;
+        }
+        return response()->json(['daftar' => $data['data'], 'status' => true], 200);
+    }
+
     public function getTagihanBayar(Request $request) {
         $client = new Client();
         $response = $client->request('GET',  config('api.url').'java-trans/tagihan-bayar-cbbl?kode_cust='.$request->query('kode'),[
---
+++ resources/views/java/fLapKartuProyek.blade.php
@@ -0,0 +1,65 @@
+<link rel="stylesheet" href="{{ asset('report.css') }}" />
+<div class="row" id="saku-filter">
+    <div class="col-12">
+        <div class="card" >
+            <x-report-header judul="Laporan Kartu Proyek" padding="px-4 py-4"/>  
+            <div class="separator"></div>
+            <div class="row">
+                <div class="col-12 col-sm-12">
+                    <div class="collapse show" id="collapseFilter">
+                        <div class="px-4 pb-4 pt-2">
+                            <form id="form-filter">
+                                <h6>Filter</h6>
+                                <div id="inputFilter">
+                                    <!-- COMPONENT -->
+                                    <x-inp-filter kode="no_proyek" nama="No Proyek" selected="3" :option="array('3')"/>
+                                    <!-- END COMPONENT -->
+                                </div>
+                                <button id="btn-tampil" style="float:right;width:110px" class="btn btn-primary ml-2 mb-3" type="submit" >Tampilkan</button>
+                                <button type="button" id="btn-tutup" class="btn btn-light mb-3" style="float:right;width:110px" type="button" >Tutup</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <x-report-paging :option="array()" default="All" />  
+            </div>                    
+        </div>
+    </div>
+</div>
+<x-report-result judul="Laporan Kartu Proyek" padding="px-0 py-4"/>
+@include('modal_search')
+@include('modal_email')
+    
+ @php
+    date_default_timezone_set("Asia/Bangkok");
+@endphp
+<script src="{{ asset('asset_dore/js/vendor/jquery.validate/sai-validate-custom.js') }}"></script>
+<script src="{{ asset('reportFilter.js') }}"></script>
+
+<script type="text/javascript">
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="-token"]').attr('content')
+        }
+    });
+    var $no_proyek = {
+        type : "=",
+        from : "",
+        fromname : "",
+        to : "",
+        toname : "",
+    }
+
+    $.ajax({
+        type: 'GET',
+        url: "{{ url('java-report/filter-kartu-tagihan') }}",
+        dataType: 'json',
+        async:false,
+        success: function(result) {
+            var initial = result.daftar[result.daftar.length - 1].no_proyek;
+            $no_proyek.from = initial;
+            $no_proyek.fromname = initial;
+            $('#no_proyek-from').val(initial);
+        }
+    })
+</script>
