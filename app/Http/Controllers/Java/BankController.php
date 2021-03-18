3558ff4fb4b05d73fe0c46fe944d5419f27c0c8e
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-03-18 10:44:14 +0700

    Update form bank java

---
+++ app/Http/Controllers/Java/BankController.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace App\Http\Controllers\Java;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Session;
+use GuzzleHttp\Exception\BadResponseException;
+
+class BankController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function __contruct(){
+        if(!Session::get('login')){
+            return redirect('java-auth/login');
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function isUnik(Request $request) {
+        try {
+            $client = new Client();
+            $response = $client->request('GET',  config('api.url').'java-master/bank?kode='.$request->query('kode'),[
+                'headers' => [
+                    'Authorization' => 'Bearer '.Session::get('token'),
+                    'Accept'     => 'application/json',
+                ]
+            ]);
+
+            if ($response->getStatusCode() == 200) { // 200 OK
+                $response_data = $response->getBody()->getContents();
+                
+                $data = json_decode($response_data,true);
+                $data = $data["data"];
+            }
+            return response()->json(['data' => $data, 'status'=>true], 200); 
+
+        } catch (BadResponseException $ex) {
+            $response = $ex->getResponse();
+            $res = json_decode($response->getBody(),true);
+            return response()->json(['message' => $res["message"], 'status'=>false], 200);
+        }
+    }
+
+    public function index(){
+        try {
+            $client = new Client();
+            $response = $client->request('GET',  config('api.url').'java-master/bank',[
+                'headers' => [
+                    'Authorization' => 'Bearer '.Session::get('token'),
+                    'Accept'     => 'application/json',
+                ]
+            ]);
+
+            if ($response->getStatusCode() == 200) { // 200 OK
+                $response_data = $response->getBody()->getContents();
+                
+                $data = json_decode($response_data,true);
+                $data = $data["data"];
+            }
+            return response()->json(['daftar' => $data, 'status'=>true], 200); 
+
+        } catch (BadResponseException $ex) {
+            $response = $ex->getResponse();
+            $res = json_decode($response->getBody(),true);
+            return response()->json(['message' => $res["message"], 'status'=>false], 200);
+        }
+    }
+
+    public function store(Request $request) {
+        $this->validate($request, [
+            'kode_bank' => 'required',
+            'nama' => 'required',
+            'no_rek' => 'required'
+        ]);
+
+        try {
+            $form = array(
+                'kode_bank' => $request->input('kode_bank'),
+                'nama' => $request->input('nama'),
+                'no_rek' => $request->input('no_rek')
+            );
+
+            $client = new Client();
+            $response = $client->request('POST',  config('api.url').'java-master/bank',[
+                'headers' => [
+                    'Authorization' => 'Bearer '.Session::get('token'),
+                    'Accept'     => 'application/json',
+                ],
+                'form_params' => $form
+            ]);
+            if ($response->getStatusCode() == 200) { // 200 OK
+                $response_data = $response->getBody()->getContents();
+                    
+                $data = json_decode($response_data,true);
+                return response()->json(['data' => $data], 200);  
+            }
+        } catch (BadResponseException $ex) {
+                $response = $ex->getResponse();
+                $res = json_decode($response->getBody(),true);
+                $data['message'] = $res;
+                $data['status'] = false;
+                return response()->json(['data' => $data], 500);
+        }
+    }
+
+    public function getData(Request $request) {
+        try{
+            $client = new Client();
+            $response = $client->request('GET',  config('api.url').'java-master/bank?kode_bank='.$request->query('kode'),
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer '.Session::get('token'),
+                    'Accept'     => 'application/json',
+                ]
+            ]);
+    
+            if ($response->getStatusCode() == 200) { // 200 OK
+                $response_data = $response->getBody()->getContents();
+                
+                $data = json_decode($response_data,true);
+            }
+            return response()->json(['data' => $data], 200); 
+        } catch (BadResponseException $ex) {
+            $response = $ex->getResponse();
+            $res = json_decode($response->getBody(),true);
+            $data['message'] = $res['message'];
+            $data['status'] = false;
+            return response()->json(['data' => $data], 200);
+        }
+    }
+
+    public function update(Request $request) {
+        $this->validate($request, [
+            'kode_bank' => 'required',
+            'nama' => 'required',
+            'no_rek' => 'required'
+        ]);
+
+        try {  
+            $form = array(
+                'kode_bank' => $request->input('kode_bank'),
+                'nama' => $request->input('nama'),
+                'no_rek' => $request->input('no_rek')
+            );
+
+            $client = new Client();
+            $response = $client->request('PUT',  config('api.url').'java-master/bank',[
+                'headers' => [
+                    'Authorization' => 'Bearer '.Session::get('token'),
+                    'Accept'     => 'application/json',
+                ],
+                'form_params' => $form
+            ]);
+            if ($response->getStatusCode() == 200) { // 200 OK
+                $response_data = $response->getBody()->getContents();
+                    
+                $data = json_decode($response_data,true);
+                return response()->json(['data' => $data], 200);  
+            }
+        } catch (BadResponseException $ex) {
+                $response = $ex->getResponse();
+                $res = json_decode($response->getBody(),true);
+                $data['message'] = $res;
+                $data['status'] = false;
+                return response()->json(['data' => $data], 500);
+        }
+    }
+
+    public function delete(Request $request) {
+        try{
+            $client = new Client();
+            $response = $client->request('DELETE',  config('api.url').'java-master/bank?kode_bank='.$request->input('kode'),
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer '.Session::get('token'),
+                    'Accept'     => 'application/json',
+                ]
+            ]);
+    
+            if ($response->getStatusCode() == 200) { // 200 OK
+                $response_data = $response->getBody()->getContents();
+                
+                $data = json_decode($response_data,true);
+            }
+            return response()->json(['data' => $data], 200); 
+        } catch (BadResponseException $ex) {
+            $response = $ex->getResponse();
+            $res = json_decode($response->getBody(),true);
+            $data['message'] = $res;
+            $data['status'] = false;
+            return response()->json(['data' => $data], 200);
+        }
+
+    }
+   
+}
