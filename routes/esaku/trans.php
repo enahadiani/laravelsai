51b1d52fb98d74aef02118ea0974c6e7e57d7511
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2021-03-22 20:02:21 +0700

    Fixed CBBL REverse Tagihan

--- routes/esaku/trans.php
+++ routes/esaku/trans.php
@@ -212,3 +212,5 @@ Route::delete('akru-simp/{no_bukti}','Esaku\Simpanan\Transaksi\AkruBillingContro
 
 /*transaksi reverse akru simpanan*/
 Route::get('reverse-akru-simp','Esaku\Simpanan\Transaksi\ReverseController@index');
+Route::get('reverse-get-anggota','Esaku\Simpanan\Transaksi\ReverseController@getAnggota');
+Route::get('reverse-akru-simp-nokartu/{no_agg}','Esaku\Simpanan\Transaksi\ReverseController@getKartuSimpanan');
