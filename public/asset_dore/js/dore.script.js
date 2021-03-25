150ad9d5e6f67888cf4b53a4cf2de2bd6c96becd
enahadiani <enahadiani2@gmail.com>
2021-03-25 17:11:10 +0700

    update dash mobile

--- public/asset_dore/js/dore.script.js
+++ public/asset_dore/js/dore.script.js
@@ -3999,7 +3999,8 @@ $.dore = function (element, options) {
       $(".theme-colors").addClass("default-transition");
       $("body > *").animate({ opacity: 1 }, 100);
       if( $("body > .logo-tarbak-overlay").length > 0){
-        $("body > .logo-tarbak-overlay").css({'display':'none','opacity':0});
+        $("body > .logo-tarbak-overlay").css({'opacity':0});
+        $("body > .logo-tarbak-overlay").hide();
       }
     }, 300);
 
