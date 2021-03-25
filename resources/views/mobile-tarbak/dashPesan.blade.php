2f4c3824e5a59e33b717caa851c67ccc8c7e2cea
enahadiani <enahadiani2@gmail.com>
2021-03-25 11:40:30 +0700

    update dash tarbak

--- resources/views/mobile-tarbak/dashPesan.blade.php
+++ resources/views/mobile-tarbak/dashPesan.blade.php
@@ -39,7 +39,7 @@
     <div class="col-12 px-0">
         <h6 class="pl-3">Pesan</h6>
         <ul class="nav nav-tabs col-12 nav-custom2" role="tablist">
-            <li class="nav-item w-50 text-center"> <a class="nav-link active" data-toggle="tab" data-href="info" href="#inbox" role="tab" aria-selected="true"><span class="hidden-xs-down">Inbox <span class="badge badge-pill badge-info" style="background: #4361EE;padding: 0.35rem;position: absolute;top: 5px;border-radius: 50%;min-width: 20px;font-size: 9px !important;min-height: 20px;">1</span></span></a> </li>
+            <li class="nav-item w-50 text-center"> <a class="nav-link active" data-toggle="tab" data-href="info" href="#inbox" role="tab" aria-selected="true"><span class="hidden-xs-down">Inbox <span class="badge badge-pill badge-info jum-read" style="background: #4361EE;padding: 0.35rem;position: absolute;top: 5px;border-radius: 50%;min-width: 20px;font-size: 9px !important;min-height: 20px;">0</span></span></a> </li>
             <li class="nav-item w-50 text-center"> <a class="nav-link" data-toggle="tab" data-href="notif" href="#notif" role="tab" aria-selected="true"><span class="hidden-xs-down">Notifikasi</span></a> </li>
         </ul>
         <div class="tab-content tabcontent-border col-12 p-0">
@@ -58,6 +58,7 @@
 </div>
 <button id="trigger-bottom-sheet" style="display:none">Bottom ?</button>
 <script>
+    getJumlahNotRead();
     var bottomSheet = new BottomSheet("country-selector");
     document.getElementById("trigger-bottom-sheet").addEventListener("click", bottomSheet.activate);
     window.bottomSheet = bottomSheet;
@@ -66,6 +67,36 @@
     $('#bottom-sheet-close').hide();
     $('#content-bottom-sheet').css({"max-height":"95vh","overflow-y":"scroll","overflow-x":"hidden"});
     $('.c-bottom-sheet__sheet').css({ "width":"100%","margin-left": "0%", "margin-right":"0%"});
+
+    function updateStatusRead(no_pesan){
+        $.ajax({
+            type: 'POST',
+            url: "{{ url('mobile-tarbak/update-status-read') }}",
+            dataType: 'json',
+            data:{'no_pesan' : no_pesan},
+            async:false,
+            success:function(result){    
+                console.log(result);
+            },
+            fail: function(xhr, textStatus, errorThrown){
+                alert('request failed:'+textStatus);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {       
+                if(jqXHR.status == 422){
+                    var msg = jqXHR.responseText;
+                }else if(jqXHR.status == 500) {
+                    var msg = "Internal server error";
+                }else if(jqXHR.status == 401){
+                    var msg = "Unauthorized";
+                    window.location="{{ url('/mobile-tarbak/sesi-habis') }}";
+                }else if(jqXHR.status == 405){
+                    var msg = "Route not valid. Page not found";
+                }
+                
+            }
+        });
+    }
+
     function getInfo(jenis){
         if(jenis == "info"){
             var url = "{{ url('mobile-tarbak/mob-info') }}";
@@ -97,7 +128,7 @@
                                 }else{
                                     var bold = "";
                                 }
-                                html+=`<div class="row mb-3 detail-pesan" data-nik_guru="`+line.nik_user+`" data-kode_matpel="`+line.kode_matpel+`">
+                                html+=`<div class="row mb-3 detail-pesan" data-nik_guru="`+line.nik_user+`" data-kode_matpel="`+line.kode_matpel+`" data-no_pesan="`+line.no_bukti+`">
                                     <div class="col-2 text-center pr-0">
                                         <img src="`+img+`" class="img-thumbnail border-0 rounded-circle list-thumbnail align-self-center xsmall" style="height:45px;width: 45px;">
                                     </div>
@@ -112,6 +143,8 @@
                                 e.preventDefault();
                                 $kode_matpel = $(this).data('kode_matpel');
                                 $nik_guru = $(this).data('nik_guru');
+                                var no_pesan = $(this).data('no_pesan');
+                                updateStatusRead(no_pesan);
                                 loadForm("{{ url('mobile-tarbak/form/dashPesanDetail') }}");
                             })
                         }else{
@@ -125,7 +158,7 @@
                                 }
                                 if(line.file_dok != "" && line.file_dok != "-" ){
                                     var img = config+"mobile-sekolah/storage/"+line.file_dok;
-                                    html+=`<div class="row mx-1 detail-notif `+bold+`" data-tanggal="`+line.tanggal+`" data-pesan="`+line.pesan+`" data-img="`+img+`">
+                                    html+=`<div class="row mx-1 detail-notif `+bold+`" data-tanggal="`+line.tanggal+`" data-pesan="`+line.pesan+`" data-img="`+img+`" data-no_pesan="`+line.no_bukti+`">
                                     <div class="col-12 col-grid">
                                     <p class="text-muted text-small mb-0" style="font-size:9px !important"><img src="{{ asset('img/mobile-tarbak/notifc.jpeg') }}" class="mr-3"> Pemberitahuan | `+line.tanggal+`</p>
                                     </div>
@@ -139,7 +172,7 @@
                                 <div class="separator mx-0"></div>`;
                                 }else{
                                     
-                                    html+=`<div class="row mx-1 detail-notif `+bold+`" data-tanggal="`+line.tanggal+`" data-pesan="`+line.pesan+`" data-img="-">
+                                    html+=`<div class="row mx-1 detail-notif `+bold+`" data-tanggal="`+line.tanggal+`" data-pesan="`+line.pesan+`" data-img="-" data-no_pesan="`+line.no_bukti+`">
                                     <div class="col-12 col-grid">
                                         <p class="text-muted text-small mb-0" style="font-size:9px !important"><img src="{{ asset('img/mobile-tarbak/notifc.jpeg') }}" class="mr-3"> Pemberitahuan | `+line.tanggal+`</p>
                                         </div>
@@ -153,10 +186,13 @@
                             $('.tab-notif').html(html+"<div style='height:100px;'>&nbsp;</div>");
                             $('.tab-notif').on('click', '.detail-notif', function(e){
                                 e.preventDefault();
+                                var no_pesan = $(this).data('no_pesan');
+                                updateStatusRead(no_pesan);
                                 $('#content-bottom-sheet').html('');
                                 var img = $(this).data('img');
                                 var tanggal = $(this).data('tanggal');
                                 var pesan = $(this).data('pesan');
+
                                 if(img == "-"){
                                     var html_img = "";
                                 }else{
