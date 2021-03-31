204dd838328d2a109162eedd537ab8266ab87e64
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-03-31 15:04:39 +0700

    Update

---
+++ app/Http/Controllers/AdmJava/PerusahaanController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Controllers\AdmJava;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Session;
+use GuzzleHttp\Exception\BadResponseException;
+
+class ProdukController extends Controller { 
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function __contruct(){
+        if(!Session::get('login')){
+            return redirect('admjava-auth/login');
+        }
+    }
+}
+?>
