60636f460880de3e2b2d5d2fa572eb9deae4c0dd
enahadiani <enahadiani2@gmail.com>
2021-03-31 16:50:58 +0700

    update billing

--- routes/ts/trans.php
+++ routes/ts/trans.php
@@ -2,3 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Session;
+
+Route::get('notif-billing-periode', 'Ts\NotifBillingController@getPeriode');
+Route::get('notif-billing-nobill', 'Ts\NotifBillingController@getNoBill');
+Route::get('notif-billing', 'Ts\NotifBillingController@index');
+Route::get('notif-billing-detail', 'Ts\NotifBillingController@show');
+Route::post('notif-billing', 'Ts\NotifBillingController@store');
+Route::post('notif-billing-ubah', 'Ts\NotifBillingController@update');
+Route::delete('notif-billing', 'Ts\NotifBillingController@destroy');
