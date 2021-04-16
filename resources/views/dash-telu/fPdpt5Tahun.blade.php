83791df02bd62cb515098f72755ddcf5fd5705a2
enahadiani <enahadiani2@gmail.com>
2021-04-16 16:13:39 +0700

    update dash telu

--- resources/views/dash-telu/fPdpt5Tahun.blade.php
+++ resources/views/dash-telu/fPdpt5Tahun.blade.php
@@ -235,12 +235,17 @@ function sepNum(x){
         }else if(!isFinite(x)){
             return 0;
         }else{
-            var x = parseFloat(x).toFixed(1);
+            var x = parseFloat(x);
             // console.log(x);
             var tmp = x.toString().split('.');
             // console.dir(tmp);
-            var y = tmp[1].substr(0,2);
-            var z = tmp[0]+'.'+y;
+            if(tmp[1] != undefined){
+
+                var y = tmp[1].substr(0,2);
+                var z = tmp[0]+'.'+y;
+            }else{
+                var z = tmp[0];
+            }
             var parts = z.split('.');
             parts[0] = parts[0].replace(/([0-9])(?=([0-9]{3})+$)/g,'$1.');
             return parts.join(',');
