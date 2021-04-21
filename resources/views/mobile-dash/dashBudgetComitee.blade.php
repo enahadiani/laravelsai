224bc31978401480b2573dc0e7602699a4fc6be7
enahadiani <enahadiani2@gmail.com>
2021-04-21 11:51:16 +0700

    update dash telu

--- resources/views/mobile-dash/dashBudgetComitee.blade.php
+++ resources/views/mobile-dash/dashBudgetComitee.blade.php
@@ -166,7 +166,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="modal-footer" style="justify-content:flex-end;width:100%">
+                    <div class="modal-footer" style="justify-content:flex-end;width:100%;border:none !important">
                         <button type="button" class="btn btn-outline-primary" id="btn-reset">Reset</button>
                         <button type="submit" class="btn btn-primary">Tampilkan</button>
                     </div>
