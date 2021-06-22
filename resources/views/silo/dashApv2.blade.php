f3a121971202ab72bd38e53e324e1bf62af23c5c
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-06-22 16:16:38 +0700

    Update dashboard apv versi 2 migrasi Silo

--- resources/views/silo/dashApv2.blade.php
+++ resources/views/silo/dashApv2.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="{{ asset('asset_silo/css/dashboard2.css') }}" />
 
-<div class="row">
+<div class="row pb-3">
     <div class="col-md-8 col-sm-12">
         <div class="row">
             <div class="col-12 mb-4">
@@ -124,11 +124,123 @@
             </div>
         </div>
     </div>
+    <div class="col-12">
+        <div class="card">
+            <h5>Daftar Transaksi</h5>
+            <div class="row">
+                <div class="col-9">
+                    <div class="search-data">
+                        <div class="append-icon">
+                            <i class="simple-icon-magnifier"></i>
+                        </div>
+                        <input name="search" class="form-control field-custom">
+                    </div>
+                </div>
+                <div class="col-3">
+                    <select class="form-control select-custom">
+                        <option value="all" selected>Tampil Semua</option>
+                    </select>
+                </div>
+                <div class="col-12 mt-3">
+                    <div class="table-scroll" id="table-scroll">
+                        <table class="table table-borderless table-condensed" id="table-data">
+                            <thead>
+                                <tr>
+                                    <th style="width: 5%;">No</th>
+                                    <th style="width: 8%;">Tanggal</th>
+                                    <th style="width: 20%;">No Bukti</th>
+                                    <th style="width: 50%;">Justifikasi Kebutuhan</th>
+                                    <th style="width: 10%;">Status Terakhir</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>22/06/2021</td>
+                                    <td>APV-2021060001</td>
+                                    <td>Catatan justifikasi kebutuhan dummy</td>
+                                    <td class="status">Approve 1</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>22/06/2021</td>
+                                    <td>APV-2021060001</td>
+                                    <td>Catatan justifikasi kebutuhan dummy</td>
+                                    <td class="status">Approve 1</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>22/06/2021</td>
+                                    <td>APV-2021060001</td>
+                                    <td>Catatan justifikasi kebutuhan dummy</td>
+                                    <td class="status">Approve 1</td>
+                                </tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>22/06/2021</td>
+                                    <td>APV-2021060001</td>
+                                    <td>Catatan justifikasi kebutuhan dummy</td>
+                                    <td class="status">Approve 1</td>
+                                </tr>
+                                <tr>
+                                    <td>5</td>
+                                    <td>22/06/2021</td>
+                                    <td>APV-2021060001</td>
+                                    <td>Catatan justifikasi kebutuhan dummy</td>
+                                    <td class="status">Approve 1</td>
+                                </tr>
+                                <tr>
+                                    <td>6</td>
+                                    <td>22/06/2021</td>
+                                    <td>APV-2021060001</td>
+                                    <td>Catatan justifikasi kebutuhan dummy</td>
+                                    <td class="status">Approve 1</td>
+                                </tr>
+                                <tr>
+                                    <td>7</td>
+                                    <td>22/06/2021</td>
+                                    <td>APV-2021060001</td>
+                                    <td>Catatan justifikasi kebutuhan dummy</td>
+                                    <td class="status">Approve 1</td>
+                                </tr>
+                                <tr>
+                                    <td>8</td>
+                                    <td>22/06/2021</td>
+                                    <td>APV-2021060001</td>
+                                    <td>Catatan justifikasi kebutuhan dummy</td>
+                                    <td class="status">Approve 1</td>
+                                </tr>
+                                <tr>
+                                    <td>9</td>
+                                    <td>22/06/2021</td>
+                                    <td>APV-2021060001</td>
+                                    <td>Catatan justifikasi kebutuhan dummy</td>
+                                    <td class="status">Approve 1</td>
+                                </tr>
+                                <tr>
+                                    <td>10</td>
+                                    <td>22/06/2021</td>
+                                    <td>APV-2021060001</td>
+                                    <td>Catatan justifikasi kebutuhan dummy</td>
+                                    <td class="status">Approve 1</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <script type="text/javascript">
     var scrollCard = document.querySelector('#monitoring');
     new PerfectScrollbar(scrollCard, {
+        suppressScrollX: true
+    });
+
+    var scrollTable = document.querySelector('#table-scroll');
+    new PerfectScrollbar(scrollTable, {
         suppressScrollX: true
     });
 </script>
