2d00a088be34f7984a3a9d00673d0bcab1ae924d
enahadiani <enahadiani2@gmail.com>
2021-06-29 15:49:40 +0700

    update siaga

---
+++ resources/views/siaga/fHistoryApp.blade.php
@@ -0,0 +1,189 @@
+<link rel="stylesheet" href="{{ asset('trans.css') }}" />
+<!-- LIST DATA -->
+<x-list-data judul="Data Histori Approval" tambah="" :thead="array('No Aju', 'No Urut', 'ID Approval', 'Keterangan', 'Tanggal', 'Aksi')" :thwidth="array(15,10,15,20,20,10)" :thclass="array('','','','','','text-center')" />
+<!-- END LIST DATA -->
+
+{{-- PRINT PREVIEW --}}
+<div id="saku-print" class="row" style="display: none;">
+    <div class="col-12">
+        <div class="card" style="height: 100%;">
+            <div class="card-body form-header" style="padding-top:1rem;padding-bottom:1rem;min-height:62.8px">
+                <button type="button" class="btn btn-light ml-2" id="btn-back" style="float:right;"><i class="fa fa-undo"></i> Kembali</button>
+                <button type="button" class="btn btn-primary ml-2" id="btn-cetak" style="float:right;"><i class="fa fa-print"></i> Print</button>
+            </div>
+            <div class="separator mb-2"></div>
+            <div class="card-body" id="print-content">
+
+            </div>
+        </div>
+    </div>
+</div>
+{{-- END PRINT PREVIEW --}}
+
+@include('modal_search')
+<script src="{{ asset('helper.js') }}"></script>
+<script type="text/javascript">
+    // SET UP VIEW
+    var scroll = document.querySelector('#content-preview');
+    new PerfectScrollbar(scroll);
+    // END SET UP VIEW
+
+    function getNamaBulan(no_bulan){
+        switch (no_bulan){
+            case 1 : case '1' : case '01': bulan = "Januari"; break;
+            case 2 : case '2' : case '02': bulan = "Februari"; break;
+            case 3 : case '3' : case '03': bulan = "Maret"; break;
+            case 4 : case '4' : case '04': bulan = "April"; break;
+            case 5 : case '5' : case '05': bulan = "Mei"; break;
+            case 6 : case '6' : case '06': bulan = "Juni"; break;
+            case 7 : case '7' : case '07': bulan = "Juli"; break;
+            case 8 : case '8' : case '08': bulan = "Agustus"; break;
+            case 9 : case '9' : case '09': bulan = "September"; break;
+            case 10 : case '10' : case '10': bulan = "Oktober"; break;
+            case 11 : case '11' : case '11': bulan = "November"; break;
+            case 12 : case '12' : case '12': bulan = "Desember"; break;
+            default: bulan = null;
+        }
+
+        return bulan;
+    }
+
+    // LIST DATA
+    var action_html = "<a href='#' title='Cetak' id='btn-print'><i class='simple-icon-printer' style='font-size:18px'></i></a>";
+    var dataTable = generateTable(
+        "table-data",
+        "{{ url('siaga-trans/app') }}", 
+        [
+            {
+                "targets": 0,
+                "createdCell": function (td, cellData, rowData, row, col) {
+                    if ( rowData.status == "baru" ) {
+                        $(td).parents('tr').addClass('selected');
+                        $(td).addClass('last-add');
+                    }
+                }
+            },
+            {'targets': 5, data: null, 'defaultContent': action_html,'className': 'text-center' }
+        ],
+        [
+            { data: 'no_bukti' },
+            { data: 'no_urut' },
+            { data: 'id' },
+            { data: 'keterangan' },
+            { data: 'tanggal' }
+        ],
+        "{{ url('siaga-auth/sesi-habis') }}",
+        [[2 ,"desc"]]
+    );
+
+    $.fn.DataTable.ext.pager.numbers_length = 5;
+
+    $("#searchData").on("keyup", function (event) {
+        dataTable.search($(this).val()).draw();
+    });
+
+    $("#page-count").on("change", function (event) {
+        var selText = $(this).val();
+        dataTable.page.len(parseInt(selText)).draw();
+    });
+
+    $('[data-toggle="popover"]').popover({ trigger: "focus" });
+    // END LIST DATA
+
+    // PRINT PREVIEW
+    function printPreview(id, jenis) {
+        $.ajax({
+            type: 'GET',
+            url: "{{ url('siaga-trans/app-preview') }}",
+            dataType: 'json',
+            data:{id:id,jenis:jenis},
+            async:false,
+            success:function(result) {
+                if(typeof result.data !== 'undefined' && result.data.length > 0) {
+                    var html = "";
+                    var no = 1;
+                    var total = 0
+                    var data = result.data[0]
+                    html += `
+                            <div class='row'>
+                                <div class='col-12 text-center' style='margin-bottom:20px;'>
+                                    <h3>TANDA TERIMA</h3>
+                                </div>    
+                                <div class='col-12'>
+                                    <table class='table table-borderless table-condensed'>
+                                        <tbody>
+                                            <tr>
+                                                <td style='width: 25%;'>ID Approval</td>
+                                                <td style='width: 75%;'>: ${data.id}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>No Justifikasi Kebutuhan</td>
+                                                <td>: ${data.no_bukti}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Tanggal</td>
+                                                <td>: ${data.tanggal}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>PP</td>
+                                                <td>: ${data.kode_pp} - ${data.nama_pp}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Keterangan</td>
+                                                <td>: ${data.keterangan}</td>
+                                            </tr>
+                                            <tr style='line-height: 40px;'>
+                                                <td>Status</td>
+                                                <td>: ${data.status}</td>
+                                            </tr>    
+                                            <tr>
+                                                <td colspan='2'>Bandung, ${data.tgl.substr(0, 2)} ${getNamaBulan(data.tgl.substr(3, 2))} ${data.tgl.substr(6, 4)}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Dibuat Oleh:</td>
+                                                <td>&nbsp;&nbsp;</td>    
+                                            </tr>
+                                            <tr style='line-height: 80px;'>
+                                                <td>Yang Menerima</td>
+                                                <td class='text-center'>Yang Menyetujui</td>    
+                                            </tr>
+                                            <tr>
+                                                <td>&nbsp;&nbsp;</td>
+                                                <td class='text-center'>${data.nik}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>    
+                                </div>
+                            </div>
+                        `;
+                        
+                    $('#print-content').html(html)
+                    $('#saku-form').hide()
+                    $('#saku-datatable').hide()
+                    $('#saku-print').show()
+                }
+            }
+        });
+    }
+
+    $('#saku-datatable').on('click','#btn-print',function(e) {
+        var id = $(this).closest('tr').find('td').eq(0).html();
+        var jenis = $(this).closest('tr').find('td').eq(2).html();
+        printPreview(id, jenis);
+    });
+
+    $('#saku-print #btn-back').click(function() {
+        $('#saku-print').hide()
+        $('#saku-datatable').show()
+        $('#saku-form').hide()
+    });
+
+    $('#saku-print #btn-cetak').click(function() {
+        $('#print-content').printThis({
+            importCSS: true,            // import parent page css
+            importStyle: true,         // import style tags
+            printContainer: true,       // print outer container/$.selector
+        });
+    });
+    // END PRINT PREVIEW
+</script>
