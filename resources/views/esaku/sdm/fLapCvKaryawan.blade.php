b27f78d39f3708e5932e0ec26d94494fa50fae16
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-07-30 09:50:57 +0700

    #SDM update form report karyawan dan cv karyawan

--- resources/views/esaku/sdm/fLapCvKaryawan.blade.php
+++ resources/views/esaku/sdm/fLapCvKaryawan.blade.php
@@ -26,7 +26,7 @@
 <div class="row" id="saku-filter">
     <div class="col-12">
         <div class="card" >
-            <x-report-header judul="Laporan Data Karyawan" padding="px-4 py-4"/>  
+            <x-report-header judul="CV Karyawan" padding="px-4 py-4"/>  
             <div class="separator"></div>
             <div class="row">
                 <div class="col-12 col-sm-12">
@@ -40,7 +40,7 @@
                                     <x-inp-filter kode="kode_gol" nama="Golongan" selected="1" :option="array('1','2','3','i')"/>
                                     <x-inp-filter kode="kode_jab" nama="Jabatan" selected="1" :option="array('1','2','3','i')"/>
                                     <x-inp-filter kode="kode_loker" nama="Lokasi Kerja" selected="1" :option="array('1','2','3','i')"/>
-                                    <x-inp-filter kode="nik" nama="NIK Karyawan" selected="1" :option="array('1','2','3','i')"/>
+                                    <x-inp-filter kode="nik" nama="NIK Karyawan" selected="1" :option="array('3')"/>
                                     <!-- END COMPONENT -->
                                 </div>
                                 <button id="btn-tampil" style="float:right;width:110px" class="btn btn-primary ml-2 mb-3" type="submit" >Tampilkan</button>
@@ -49,13 +49,13 @@
                         </div>
                     </div>
                 </div>
-                <x-report-paging :option="array()" default="10" />  
+                <x-report-paging :option="array(1)" default="1" />  
             </div>                    
         </div>
     </div>
 </div>
 
-<x-report-result judul="Laporan Data Karyawan" padding="px-0 py-4"/>
+<x-report-result judul="CV Karyawan" padding="px-0 py-4"/>
 @include('modal_search')
 @include('modal_email')
     
@@ -105,12 +105,13 @@ var $kode_loker = {
 }
 
 var $nik = {
-    type : "all",
-    from : "",
-    fromname : "",
+    type : "=",
+    from : "{{ Session::get('userLog') }}",
+    fromname : "{{ Session::get('userLog') }}",
     to : "",
     toname : "",
 }
+$('#nik-from').val("{{ Session::get('userLog') }}")
 
 $('#btn-filter').click(function(e){
     $('#collapseFilter').show();
@@ -277,101 +278,32 @@ $('#form-filter').submit(function(e){
         console.log(pair[0]+ ', '+ pair[1]); 
     }
     $('#saku-report').removeClass('hidden');
-    xurl = "{{ url('esaku-auth/form/sdm_rptKaryawan') }}";
+    xurl = "{{ url('esaku-auth/form/sdm_rptCvKaryawan') }}";
     $('#saku-report #canvasPreview').load(xurl);
 });
 
-$('#show').change(function(e){
-    $formData = new FormData();
-    $formData.append("kode_sdm[]",$kode_sdm.type);
-    $formData.append("kode_sdm[]",$kode_sdm.from);
-    $formData.append("kode_sdm[]",$kode_sdm.to);
-    $formData.append("kode_gol[]",$kode_gol.type);
-    $formData.append("kode_gol[]",$kode_gol.from);
-    $formData.append("kode_gol[]",$kode_gol.to);
-    $formData.append("kode_jab[]",$kode_jab.type);
-    $formData.append("kode_jab[]",$kode_jab.from);
-    $formData.append("kode_jab[]",$kode_jab.to);
-    $formData.append("kode_loker[]",$kode_loker.type);
-    $formData.append("kode_loker[]",$kode_loker.from);
-    $formData.append("kode_loker[]",$kode_loker.to);
-    $formData.append("nik[]",$nik.type);
-    $formData.append("nik[]",$nik.from);
-    $formData.append("nik[]",$nik.to);
-    for(var pair of $formData.entries()) {
-        console.log(pair[0]+ ', '+ pair[1]); 
-    }
-    $('#saku-report').removeClass('hidden');
-    xurl = "{{ url('esaku-auth/form/sdm_rptKaryawan') }}";
-    $('#saku-report #canvasPreview').load(xurl);
-});
-
-$('#saku-report #canvasPreview').on('click', '.karyawan', function(e){
-    e.preventDefault();
-    var nik = $(this).data('karyawan');
-    var back = true;
-    
-    $formData = new FormData();
-    $formData.append("kode_sdm[]",$kode_sdm.type);
-    $formData.append("kode_sdm[]",$kode_sdm.from);
-    $formData.append("kode_sdm[]",$kode_sdm.to);
-    $formData.append("kode_gol[]",$kode_gol.type);
-    $formData.append("kode_gol[]",$kode_gol.from);
-    $formData.append("kode_gol[]",$kode_gol.to);
-    $formData.append("kode_jab[]",$kode_jab.type);
-    $formData.append("kode_jab[]",$kode_jab.from);
-    $formData.append("kode_jab[]",$kode_jab.to);
-    $formData.append("kode_loker[]",$kode_loker.type);
-    $formData.append("kode_loker[]",$kode_loker.from);
-    $formData.append("kode_loker[]",$kode_loker.to);
-    $formData.append("nik[]","=");
-    $formData.append("nik[]",nik);
-    $formData.append("nik[]",$nik.to);
-
-    $formData.delete('back');
-    $formData.append('back', back);
-    $('.breadcrumb').html('');
-    $('.breadcrumb').append(`
-        <li class="breadcrumb-item">
-            <a href="#" class="klik-report" data-href="laporan-karyawan" aria-param="">Laporan Data Karyawan</a>
-        </li>
-        <li class="breadcrumb-item active" aria-current="cv-karyawan" aria-param="`+nik+`">Curiculum Vitae Karyawan</li>
-    `);
-    xurl ="{{ url('esaku-auth/form/sdm_rptCvKaryawan') }}";
-    $('#saku-report #canvasPreview').load(xurl);
-});
-
-$('.navigation-lap').on('click', '#btn-back', function(e){
-    e.preventDefault();
-    $formData = new FormData();
-    $formData.append("kode_sdm[]",$kode_sdm.type);
-    $formData.append("kode_sdm[]",$kode_sdm.from);
-    $formData.append("kode_sdm[]",$kode_sdm.to);
-    $formData.append("kode_gol[]",$kode_gol.type);
-    $formData.append("kode_gol[]",$kode_gol.from);
-    $formData.append("kode_gol[]",$kode_gol.to);
-    $formData.append("kode_jab[]",$kode_jab.type);
-    $formData.append("kode_jab[]",$kode_jab.from);
-    $formData.append("kode_jab[]",$kode_jab.to);
-    $formData.append("kode_loker[]",$kode_loker.type);
-    $formData.append("kode_loker[]",$kode_loker.from);
-    $formData.append("kode_loker[]",$kode_loker.to);
-    $formData.append("nik[]",$nik.type);
-    $formData.append("nik[]",$nik.from);
-    $formData.append("nik[]",$nik.to);
-
-    var aktif = $('.breadcrumb-item.active').attr('aria-current');
-    var tmp = $('.breadcrumb-item.active').attr('aria-param').split("|");
-    var param = tmp[0];
-    if(aktif == "cv-karyawan"){
-        $formData.delete('back');
-        xurl = "{{ url('esaku-auth/form/sdm_rptKaryawan') }}";
-        $('.breadcrumb').html('');
-        $('.breadcrumb').append(`
-            <li class="breadcrumb-item active" aria-current="laporan-karyawan" aria-param="">LLaporan Data Karyawan</li>
-        `);
-        $('.navigation-lap').addClass('hidden');
-    }
-    $('#saku-report #canvasPreview').load(xurl);
-});
+    $('#show').change(function(e){
+        $formData = new FormData();
+        $formData.append("kode_sdm[]",$kode_sdm.type);
+        $formData.append("kode_sdm[]",$kode_sdm.from);
+        $formData.append("kode_sdm[]",$kode_sdm.to);
+        $formData.append("kode_gol[]",$kode_gol.type);
+        $formData.append("kode_gol[]",$kode_gol.from);
+        $formData.append("kode_gol[]",$kode_gol.to);
+        $formData.append("kode_jab[]",$kode_jab.type);
+        $formData.append("kode_jab[]",$kode_jab.from);
+        $formData.append("kode_jab[]",$kode_jab.to);
+        $formData.append("kode_loker[]",$kode_loker.type);
+        $formData.append("kode_loker[]",$kode_loker.from);
+        $formData.append("kode_loker[]",$kode_loker.to);
+        $formData.append("nik[]",$nik.type);
+        $formData.append("nik[]",$nik.from);
+        $formData.append("nik[]",$nik.to);
+        for(var pair of $formData.entries()) {
+            console.log(pair[0]+ ', '+ pair[1]); 
+        }
+        $('#saku-report').removeClass('hidden');
+        xurl = "{{ url('esaku-auth/form/sdm_rptCvKaryawan') }}";
+        $('#saku-report #canvasPreview').load(xurl);
+    });
 </script>
