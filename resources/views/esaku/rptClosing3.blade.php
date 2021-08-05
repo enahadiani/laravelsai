ae6d8dc83a7bbf78b96a8117f42465a446a039ed
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-08-05 12:57:15 +0700

    #INV update laporan closing

--- resources/views/esaku/rptClosing3.blade.php
+++ resources/views/esaku/rptClosing3.blade.php
@@ -19,82 +19,190 @@ function drawRptPage(data,res,from,to){
     var arr_tl = [0,0,0,0,0,0,0,0,0];
     var x=1;
     if(data.length > 0) {
-        html += "<div id='sai-rpt-table-export-tbl-daftar-pnj'>";
+        var no_id = 1;
+        var _total = [];
+        html = `<div id="sai-rpt-table-export-tbl-daftar-pnj">`;
         for(var i=0;i<data.length;i++) {
-            var value = data[i];
-            var diskon=0;
-            var total=0;  
-            var subTot=0;
-            var no=1;
-            html += "<div class='card card-body'>";
-            html += "<h3><b>No Closing</b> <span class='pull-right'>#"+value.no_bukti+"</span></h3>";
-            html += "<hr/>";
-            html += "<div class='row'>";
-            html += "<div class='col-md-12'>";
-            html += "<div class='pull-left'>";
-            html += "<p class='text-muted m-l-5'>";
-            html += value.tanggal+"<br/>";
-            html += "Saldo Awal: "+sepNum(value.saldo_awal);
-            html += "<br/>Total Penjualan: "+sepNum(value.total_pnj);
-            html += "<br/>"+value.nik_user;
-            html += "</p>";
-            html += "</div>"
-            html += "</div>";
-            html += "<div class='col-md-12'>";
-            html += "<div class='table-responsive mt-40' style='clear: both;'>";
-            html += "<table class='table table-hover'>";
-            html += "<thead>";
-            html += "<tr>";
-            html += "<th class='text-center'>No</th>";
-            html += "<th class='text-center'>No Bukti</th>";
-            html += "<th class='text-center'>No Jual</th>";
-            html += "<th class='text-center'>Periode</th>";
-            html += "<th class='text-center'>Tanggal</th>";
-            html += "<th class='text-right'>Diskon</th>";
-            html += "<th class='text-right'>Nilai</th>";
-            html += "<th>Keterangan</th>";
-            html += "</tr>";
-            html += "</thead>";
-            html += "<tbody>";
-            for (var j=0;j<res.res.data_detail.length;j++) {
-                var value2 = res.res.data_detail[j];
-                if(value.no_bukti == value2.no_bukti){
-                    diskon+=+value2.diskon;
-                    total+=+value2.nilai;
-                    subTot+= +parseFloat(value2.total)+parseFloat(value2.diskon);
-                    html += "<tr>"
-                    html += "<td align='center' class='isi_laporan'>"+no+"</td>";
-                    html += "<td align='center'  class='isi_laporan'>"+value2.no_bukti+"</td>";
-                    html += "<td align='center' class='isi_laporan'>"+value2.no_jual+"</td>";
-                    html += "<td align='center' class='isi_laporan'>"+value2.periode+"</td>";
-                    html += "<td align='center' class='isi_laporan'>"+value2.tanggal+"</td>";
-                    html += "<td align='right' class='isi_laporan'>"+sepNum(value2.diskon)+"</td>";
-                    html += "<td align='right' class='isi_laporan'>"+sepNum(value2.nilai)+"</td>";
-                    html += "<td align='left' class='isi_laporan'>"+value2.keterangan+"</td>";
-                    html += "</tr>";
-                    no++;
-                }
-            }
-            html += "</tbody>";
-            html += "</table>";
-            html += "</div>";
-            html += "</div>"
-            html += "<div class='col-md-12'>";
-            html += "<div class='pull-right m-t-30 text-right'>";
-            html += "<p>Discount: "+sepNum(diskon)+"</p>";
-            html += "<hr/>";
-            html += "<h3><b>Total: </b>"+sepNum(total)+"</h3>"
-            html += "</div>";
-            html += "<div class='clearfix'></div>"
-            html += "</div>";
-            html += "</div>";
-            html += "</div>";
+            var row = data[i];
+            var diskon = 0;
+            var total = 0;
+            var subTot = 0;
+            var no = 1;
+
+            html += `<div class="card card-body" style="margin-bottom: 32px;">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="pull-left">
+                            <h3>
+                                <b>No Closing</b>
+                                <span class="pull-right">#${row.no_bukti_close}</span>
+                            </h3>
+                            <hr />
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td style="width: 201px;">No Open Kasir</td>
+                                        <td>: ${row.no_bukti_open}</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 201px;">Waktu</td>
+                                        <td>: ${row.tanggal_open} ${row.jam_open}</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 201px;">No Closing Kasir</td>
+                                        <td>: ${row.no_bukti_close}</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 201px;">Waktu</td>
+                                        <td>: ${row.tanggal_close} ${row.jam_close}</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 201px;">Saldo Awal</td>
+                                        <td>: ${sepNum(row.saldo_awal)}</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 201px;">Total Penjualan</td>
+                                        <td>: ${sepNum(row.total_pnj)}</td>
+                                    </tr>    
+                                    <tr>
+                                        <td style="width: 201px;">Total</td>
+                                        <td>: <span id="total-ke-${no_id}"></span></td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 201px;">Kasir</td>
+                                        <td>: ${row.nik_user}</td>
+                                    </tr>    
+                                </tbody>
+                            </table>      
+                        </div>    
+                    </div>   
+                    <div class="col-12">
+                        <div class="table-responsive m-t-40" style="clear: both;">
+                            <table class="table table-hover">
+                                <thead>
+                                    <th class="text-center">No</th>    
+                                    <th class="text-center">No Bukti</th>    
+                                    <th class="text-center">No Jual</th>    
+                                    <th class="text-center">Periode</th>    
+                                    <th class="text-center">Tanggal</th>    
+                                    <th class="text-center">Diskon</th>    
+                                    <th class="text-center">Nilai</th>    
+                                    <th class="text-center">Keterangan</th>    
+                                </thead>
+                                <tbody>`
+                                for(var j=0;j<res.res.data_detail.length;j++) {
+                                    var detail = res.res.data_detail[j]
+                                    if(row.no_bukti_close == detail.no_bukti) {
+                                        diskon +=+ parseFloat(detail.diskon);
+                                        total +=+ parseFloat(detail.nilai);
+                                        subTot +=+ parseFloat(detail.nilai) + parseFloat(detail.diskon);
+                                        html += `<tr>
+                                            <td class="text-center isi-laporan">${no}</td>    
+                                            <td class="text-center isi-laporan">${detail.no_bukti}</td>    
+                                            <td class="text-center isi-laporan">${detail.no_jual}</td>    
+                                            <td class="text-center isi-laporan">${detail.periode}</td>    
+                                            <td class="text-center isi-laporan">${detail.tanggal}</td>    
+                                            <td class="text-left isi-laporan">${sepNum(detail.diskon)}</td>    
+                                            <td class="text-left isi-laporan">${sepNum(detail.nilai)}</td>    
+                                            <td class="text-center isi-laporan">${detail.keterangan}</td>    
+                                        </tr>`
+                                        no++;
+                                    }
+                                }
+                        _total.push(total);
+                        html += `</tbody>    
+                            </table>    
+                        </div>    
+                    </div> 
+                    <div class="clearfix"></div>
+                </div>
+            </div>`
+            no_id++
         }
-        html += "<div style='page-break-after:always'></div>";
-        html += "</div>";
+        html += `<div style='page-break-after:always'></div>
+        </div>`;
+        // html += "<div id='sai-rpt-table-export-tbl-daftar-pnj'>";
+        // for(var i=0;i<data.length;i++) {
+        //     var value = data[i];
+        //     var diskon=0;
+        //     var total=0;  
+        //     var subTot=0;
+        //     var no=1;
+        //     html += "<div class='card card-body'>";
+        //     html += "<h3><b>No Closing</b> <span class='pull-right'>#"+value.no_bukti+"</span></h3>";
+        //     html += "<hr/>";
+        //     html += "<div class='row'>";
+        //     html += "<div class='col-md-12'>";
+        //     html += "<div class='pull-left'>";
+        //     html += "<p class='text-muted m-l-5'>";
+        //     html += value.tanggal+"<br/>";
+        //     html += "Saldo Awal: "+sepNum(value.saldo_awal);
+        //     html += "<br/>Total Penjualan: "+sepNum(value.total_pnj);
+        //     html += "<br/>"+value.nik_user;
+        //     html += "</p>";
+        //     html += "</div>"
+        //     html += "</div>";
+        //     html += "<div class='col-md-12'>";
+        //     html += "<div class='table-responsive mt-40' style='clear: both;'>";
+        //     html += "<table class='table table-hover'>";
+        //     html += "<thead>";
+        //     html += "<tr>";
+        //     html += "<th class='text-center'>No</th>";
+        //     html += "<th class='text-center'>No Bukti</th>";
+        //     html += "<th class='text-center'>No Jual</th>";
+        //     html += "<th class='text-center'>Periode</th>";
+        //     html += "<th class='text-center'>Tanggal</th>";
+        //     html += "<th class='text-right'>Diskon</th>";
+        //     html += "<th class='text-right'>Nilai</th>";
+        //     html += "<th>Keterangan</th>";
+        //     html += "</tr>";
+        //     html += "</thead>";
+        //     html += "<tbody>";
+        //     for (var j=0;j<res.res.data_detail.length;j++) {
+        //         var value2 = res.res.data_detail[j];
+        //         if(value.no_bukti == value2.no_bukti){
+        //             diskon+=+value2.diskon;
+        //             total+=+value2.nilai;
+        //             subTot+= +parseFloat(value2.total)+parseFloat(value2.diskon);
+        //             html += "<tr>"
+        //             html += "<td align='center' class='isi_laporan'>"+no+"</td>";
+        //             html += "<td align='center'  class='isi_laporan'>"+value2.no_bukti+"</td>";
+        //             html += "<td align='center' class='isi_laporan'>"+value2.no_jual+"</td>";
+        //             html += "<td align='center' class='isi_laporan'>"+value2.periode+"</td>";
+        //             html += "<td align='center' class='isi_laporan'>"+value2.tanggal+"</td>";
+        //             html += "<td align='right' class='isi_laporan'>"+sepNum(value2.diskon)+"</td>";
+        //             html += "<td align='right' class='isi_laporan'>"+sepNum(value2.nilai)+"</td>";
+        //             html += "<td align='left' class='isi_laporan'>"+value2.keterangan+"</td>";
+        //             html += "</tr>";
+        //             no++;
+        //         }
+        //     }
+        //     html += "</tbody>";
+        //     html += "</table>";
+        //     html += "</div>";
+        //     html += "</div>"
+        //     html += "<div class='col-md-12'>";
+        //     html += "<div class='pull-right m-t-30 text-right'>";
+        //     html += "<p>Discount: "+sepNum(diskon)+"</p>";
+        //     html += "<hr/>";
+        //     html += "<h3><b>Total: </b>"+sepNum(total)+"</h3>"
+        //     html += "</div>";
+        //     html += "<div class='clearfix'></div>"
+        //     html += "</div>";
+        //     html += "</div>";
+        //     html += "</div>";
+        // }
+        // html += "<div style='page-break-after:always'></div>";
+        // html += "</div>";
     }
     $('#canvasPreview').html(html);
     $('li.prev a ').html("<i class='simple-icon-arrow-left'></i>");
     $('li.next a ').html("<i class='simple-icon-arrow-right'></i>");
+
+    var _id = 1;
+    for(var x=0;x<_total.length;x++) {
+        $(`#total-ke-${_id}`).text(sepNum(_total[x]))
+        _id++;
+    }
 }
 </script>
