f9dbce58d3f6f25cafd44b374661af8aae43510f
enahadiani <enahadiani2@gmail.com>
2021-09-27 09:49:11 +0700

    upd req mas deni ppn beli tidak dihitung ke nilai transaksi

--- resources/views/esaku/fPembelian2.blade.php
+++ resources/views/esaku/fPembelian2.blade.php
@@ -612,8 +612,8 @@
         var total = totrans - todisk;
         var ppn = (total * 10)/100;
         $("#toppn").val(toRp(ppn));
-        var total2 = total+ppn;
-        $("#tostlh").val(toRp(total2));
+        // var total2 = total+ppn;
+        // $("#tostlh").val(toRp(total2));
     }
 
     function hitungDisc(){
@@ -645,8 +645,9 @@
 
         var total_disk= toNilai($('#todisk').val());
         var total_stlh = +total_brg - +total_disk;
-        var total_ppn = toNilai($('#toppn').val());
-        var total = total_stlh + total_ppn; 
+        // var total_ppn = toNilai($('#toppn').val());
+        // var total = total_stlh + total_ppn; 
+        var total = total_stlh;
         $('#tostlh').val(toRp(total));   
     }
 
