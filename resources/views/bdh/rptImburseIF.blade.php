bd0fdc9dd345ba8bcc57dae84a4177bbbe218448
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-09-30 14:11:55 +0700

    #BDH update report link laporan IF

--- resources/views/bdh/rptImburseIF.blade.php
+++ resources/views/bdh/rptImburseIF.blade.php
@@ -19,6 +19,12 @@ function drawRptPage(data,res,from,to) {
     console.log(res)
     if(data.length > 0) {
         var resData = res.res
+        if(resData.back){
+            $('.navigation-lap').removeClass('hidden');
+        }else{
+            $('.navigation-lap').addClass('hidden');
+        }
+        
         for(var i=0;i<data.length;i++) {
             var no = 1;
             var no2 = 1;
