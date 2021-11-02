b4c24d7c4f3c4bb07374e18e5a442205c2689f04
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-11-02 09:53:21 +0700

    #DASHYPT update dashboard FP

--- resources/views/dash-ypt/fFinancial.blade.php
+++ resources/views/dash-ypt/fFinancial.blade.php
@@ -704,6 +704,7 @@ $('#list-filter-2').on('click', 'div', function(event) {
 
     $('#select-text-fp').text(`${$filter2.toUpperCase()} || ${$tahun}`)
     updateAllChart()
+    showNotification(`${$filter2.toUpperCase()} ${$tahun}`);
 })
 // END FILTER EVENT
 // MENAMPILKAN LIST CUSTOM EXPORT HIGHCHART
@@ -1045,10 +1046,6 @@ function updateChart() {
 
             lrChart.series[1].update({
                 data: data.data_beban
-            }, false) // true untuk redraw
-
-            lrChart.series[2].update({
-                data: data.data_shu
             }, false) // true untuk redraw
 
             // re render chart
@@ -2974,4 +2971,54 @@ $('#export-akun.menu-chart-custom ul li').click(function(event) {
 })
 // END PER AKUN
 // END CUSTOM EXPORT HIGHCHART
+
+// CONFIG FUNCTION
+function showNotification(message) {
+    $.notify(
+        {
+            title: 'Update',
+            message: `Menampilkan dashboard periode ${message}`,
+            target: "_blank"
+        },
+        {
+            element: "body",
+            position: null,
+            type: 'success',
+            allow_dismiss: true,
+            newest_on_top: false,
+            showProgressbar: false,
+            placement: {
+                from: 'top',
+                align: 'center'
+            },
+            offset: 20,
+            spacing: 10,
+            z_index: 1031,
+            delay: 4000,
+            timer: 2000,
+            url_target: "_blank",
+            mouse_over: null,
+            animate: {
+                enter: "animated fadeInDown",
+                exit: "animated fadeOutUp"
+            },
+            onShow: null,
+            onShown: null,
+            onClose: null,
+            onClosed: null,
+            icon_type: "class",
+            template: `<div data-notify="container" class="col-11 col-sm-3 alert  alert-{0} " role="alert">
+                <button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>
+                <span data-notify="icon"></span>
+                <span data-notify="title">{1}</span>
+                <span data-notify="message">{2}</span>
+                <div class="progress" data-notify="progressbar">
+                    <div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>
+                </div>
+                <a href="{3}" target="{4}" data-notify="url"></a>
+                </div>`
+        }
+    );
+}
+// END CONFIG FUNCTION
 </script>
