edf83db05376efd6999e39606278c37dcf7540d8
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2021-12-01 08:26:52 +0800

    #kontrak updated

--- app/Http/Controllers/Esaku/Sdm/KepegawaianV3Controller.php
+++ app/Http/Controllers/Esaku/Sdm/KepegawaianV3Controller.php
@@ -312,27 +312,7 @@ class Kepegawaianv3Controller extends Controller
     public function store_kontrak(Request $request)
     {
         $this->validate($request, [
-            'nik' => 'required',
-            'nama' => 'required',
-            'no_ktp' => 'required',
-            'jk' => 'required',
-            'kode_agama' => 'required',
-            'no_telp' => 'required',
-            'no_hp' => 'required',
-            'tgl_lahir' => 'required',
-            'alamat' => 'required',
-            'provinsi' => 'required',
-            'kota' => 'required',
-            'kecamatan' => 'required',
-            'kelurahan' => 'required',
-            'kode_pos' => 'required',
-            't_badan' => 'required',
-            'b_badan' => 'required',
-            'gol_darah' => 'required',
-            'no_kk' => 'required',
-            'status_nikah' => 'required',
-            'tgl_nikah' => 'required',
-            'kode_gol' => 'required',
+            'kode_status' => 'required',
             'kode_sdm' => 'required',
             'kode_loker' => 'required',
             'tgl_masuk' => 'required',
@@ -340,10 +320,6 @@ class Kepegawaianv3Controller extends Controller
             'no_bpjs' => 'required',
             'no_bpjs_kerja' => 'required',
             'kode_profesi' => 'required',
-            'kode_bank' => 'required',
-            'cabang' => 'required',
-            'no_rek' => 'required',
-            'nama_rek' => 'required',
             'client' => 'required',
 
             'skill' => 'required',
@@ -361,91 +337,12 @@ class Kepegawaianv3Controller extends Controller
                     "contents" => $request->input('nik')
                 ),
                 array(
-                    "name" => "nama",
-                    "contents" => $request->input('nama')
-                ),
-                array(
-                    "name" => "nomor_ktp",
-                    "contents" => $request->input('no_ktp')
-                ),
-                array(
-                    "name" => "jenis_kelamin",
-                    "contents" => $request->input('jk')
-                ),
-                array(
-                    "name" => "kode_agama",
-                    "contents" => $request->input('kode_agama')
-                ),
-                array(
-                    "name" => "no_telp",
-                    "contents" => $request->input('no_telp')
-                ),
-                array(
-                    "name" => "no_hp",
-                    "contents" => $request->input('no_hp')
-                ),
-                array(
-                    "name" => "tempat_lahir",
-                    "contents" => $request->input('tempat')
-                ),
-                array(
-                    "name" => "tgl_lahir",
-                    "contents" => $this->convertDate($request->input('tgl_lahir'))
-                ),
-                array(
-                    "name" => "alamat",
-                    "contents" => $request->input('alamat')
-                ),
-                array(
-                    "name" => "provinsi",
-                    "contents" => $request->input('provinsi')
-                ),
-                array(
-                    "name" => "kota",
-                    "contents" => $request->input('kota')
-                ), array(
-                    "name" => "kecamatan",
-                    "contents" => $request->input('kecamatan')
-                ),
-                array(
-                    "name" => "kelurahan",
-                    "contents" => $request->input('kelurahan')
-                ),
-                array(
-                    "name" => "kode_pos",
-                    "contents" => $request->input('kode_pos')
-                ),
-                array(
-                    "name" => "tinggi_badan",
-                    "contents" => $this->joinNum($request->input('t_badan'))
-                ),
-                array(
-                    "name" => "berat_badan",
-                    "contents" => $this->joinNum($request->input('b_badan'))
-                ),
-                array(
-                    "name" => "golongan_darah",
-                    "contents" => $request->input('gol_darah')
-                ),
-                array(
-                    "name" => "nomor_kk",
-                    "contents" => $request->input('no_kk')
-                ),
-                array(
-                    "name" => "status_nikah",
-                    "contents" => $request->input('status_nikah')
-                ),
-                array(
-                    "name" => "tgl_nikah",
-                    "contents" => $this->convertDate($request->input('tgl_nikah'))
-                ),
-                array(
-                    "name" => "kode_golongan",
-                    "contents" => $request->input('kode_gol')
-                ),
-                array(
                     "name" => "kode_sdm",
                     "contents" => $request->input('kode_sdm')
+                ),
+                array(
+                    "name" => "kode_status",
+                    "contents" => $request->input('kode_status')
                 ),
                 array(
                     "name" => "kode_loker",
@@ -535,87 +432,9 @@ class Kepegawaianv3Controller extends Controller
                 )
             );
 
-            $array_nomor = array();
-            $array_jenis = array();
-            $array_status = array();
-            $array_fileName = array();
-            $array_filePrevName = array();
-            $array_isUpload = array();
-
-            if (count($request->input('nu')) > 0) {
-                for ($i = 0; $i < count($request->input('nu')); $i++) {
-                    $data_nomor = array(
-                        "name" => "nu[]",
-                        "contents" => $request->nu[$i]
-                    );
-                    $data_jenis = array(
-                        "name" => "jenis[]",
-                        "contents" => $request->jenis[$i]
-                    );
-                    $data_status = array(
-                        "name" => "sts_dokumen[]",
-                        "contents" => $request->sts_dokumen[$i]
-                    );
-                    $data_fileName = array(
-                        "name" => "fileName[]",
-                        "contents" => $request->fileName[$i]
-                    );
-                    $data_filePrevName = array(
-                        "name" => "filePrevName[]",
-                        "contents" => $request->filePrevName[$i]
-                    );
-                    $data_isUpload = array(
-                        "name" => "isUpload[]",
-                        "contents" => $request->isUpload[$i]
-                    );
-
-                    array_push($array_nomor, $data_nomor);
-                    array_push($array_jenis, $data_jenis);
-                    array_push($array_status, $data_status);
-                    array_push($array_fileName, $data_fileName);
-                    array_push($array_filePrevName, $data_filePrevName);
-                    array_push($array_isUpload, $data_isUpload);
-                }
-
-                $fields = array_merge(
-                    $fields,
-                    $array_nomor,
-                    $array_jenis,
-                    $array_status,
-                    $array_fileName,
-                    $array_filePrevName,
-                    $array_isUpload
-                );
-            }
-
-            if (!empty($request->file('file'))) {
-                if (count($request->file('file')) > 0) {
-                    $array_file = array();
-                    for ($i = 0; $i < count($request->file('file')); $i++) {
-                        if (isset($request->file('file')[$i])) {
-                            $image_path = $request->file('file')[$i]->getPathname();
-                            $image_mime = $request->file('file')[$i]->getmimeType();
-                            $image_org  = $request->file('file')[$i]->getClientOriginalName();
-
-                            $data_file = array(
-                                'name'     => 'file[]',
-                                'filename' => $image_org,
-                                'Mime-Type' => $image_mime,
-                                'contents' => fopen($image_path, 'r')
-                            );
-
-                            array_push($array_file, $data_file);
-                        }
-                    }
-
-                    $fields = array_merge($fields, $array_file);
-                }
-            }
-
-
             // dd($fields);
             $client = new Client();
-            $response = $client->request('POST',  config('api.url') . 'esaku-trans/v3/sdm-karyawan', [
+            $response = $client->request('POST',  config('api.url') . 'esaku-trans/v3/sdm-kontrak', [
                 'headers' => [
                     'Authorization' => 'Bearer ' . Session::get('token'),
                     'Accept'     => 'application/json',
