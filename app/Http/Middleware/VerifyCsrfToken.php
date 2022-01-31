313ef013cc3a3b189b4797e2a87d03c6b641039f
enahadiani <enahadiani2@gmail.com>
2022-01-31 13:42:28 +0700

    update

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'midtrans/*'
-     ];
+        'midtrans/*',
+        'siaga-auth/autologin'
+    ];
 }
