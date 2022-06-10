9553c79b375cd33e51fa5d7c48732eddf6ff3ba3
enahadiani <devsaku5@gmail.com>
2022-06-10 13:07:15 +0700

    update dash pbh

---
+++ app/Http/Controllers/DashTelu/DashboardPBHController.php
@@ -0,0 +1,104 @@
+<?php
+namespace App\Http\Controllers\DashTelu;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Session;
+use GuzzleHttp\Exception\BadResponseException;
+
+class DashboardPBHController extends Controller {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function __contruct(){
+        if(!Session::get('login')){
+            return redirect('dash-telu/login');
+        }
+    }
+
+    public function getDataBox(Request $r) {
+        try {
+            
+            $client = new Client();
+            $response = $client->request('GET',  config('api.url').'ypt-dash/data-pbh-box',[
+                'headers' => [
+                    'Authorization' => 'Bearer '.Session::get('token'),
+                    'Accept'     => 'application/json',
+                ],
+                'query' => $r->query()
+            ]);
+
+            if ($response->getStatusCode() == 200) { // 200 OK
+                $response_data = $response->getBody()->getContents();
+                
+                $data = json_decode($response_data,true);
+            }
+            return response()->json($data, 200); 
+
+        } catch (BadResponseException $ex) {
+            $response = $ex->getResponse();
+            $res = json_decode($response->getBody(),true);
+            return response()->json(['message' => $res["message"], 'status'=>false], 200);
+        }
+    }
+
+    public function getJenisPengajuan(Request $r) {
+        try {
+            
+            $client = new Client();
+            $response = $client->request('GET',  config('api.url').'ypt-dash/data-pbh-jenis-aju',[
+                'headers' => [
+                    'Authorization' => 'Bearer '.Session::get('token'),
+                    'Accept'     => 'application/json',
+                ],
+                'query' => $r->query()
+            ]);
+
+            if ($response->getStatusCode() == 200) { // 200 OK
+                $response_data = $response->getBody()->getContents();
+                
+                $data = json_decode($response_data,true);
+            }
+            return response()->json($data, 200); 
+
+        } catch (BadResponseException $ex) {
+            $response = $ex->getResponse();
+            $res = json_decode($response->getBody(),true);
+            return response()->json(['message' => $res["message"], 'status'=>false], 200);
+        }
+    }
+
+    public function getNilaiKas(Request $r) {
+        try {
+            
+            $client = new Client();
+            $response = $client->request('GET',  config('api.url').'ypt-dash/data-pbh-kas',[
+                'headers' => [
+                    'Authorization' => 'Bearer '.Session::get('token'),
+                    'Accept'     => 'application/json',
+                ],
+                'query' => $r->query()
+            ]);
+
+            if ($response->getStatusCode() == 200) { // 200 OK
+                $response_data = $response->getBody()->getContents();
+                
+                $data = json_decode($response_data,true);
+            }
+            return response()->json($data, 200); 
+
+        } catch (BadResponseException $ex) {
+            $response = $ex->getResponse();
+            $res = json_decode($response->getBody(),true);
+            return response()->json(['message' => $res["message"], 'status'=>false], 200);
+        }
+    }
+
+   
+}
+?>
--- routes/telu/dash.php
+++ routes/telu/dash.php
@@ -150,4 +150,8 @@ Route::get('video-list','DashTelu\DashboardController@getVideoList');
 Route::get('financial-target','DashTelu\DashboardController@getTarget');
 Route::post('note','DashTelu\DashboardController@updateNoteTarget');
 
+Route::get('data-pbh-box','DashTelu\DashboardPBHController@getDataBox');
+Route::get('data-pbh-jenis-aju','DashTelu\DashboardPBHController@getJenisPengajuan');
+Route::get('data-pbh-kas','DashTelu\DashboardPBHController@getNilaiKas');
+
 ?>
