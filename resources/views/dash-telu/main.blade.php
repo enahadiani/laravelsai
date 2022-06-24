5598e3f1a16d7b8080a280ef0cb5377941fe5a63
enahadiani <devsaku5@gmail.com>
2022-06-24 09:22:58 +0700

    update

--- resources/views/dash-telu/main.blade.php
+++ resources/views/dash-telu/main.blade.php
@@ -276,7 +276,7 @@
         </a>
         @php
             $tmp = explode(" ",Session::get('namaUser'));
-            $nama = $tmp[0]." ".$tmp[1];
+            $nama = $tmp[0]." ".(isset($tmp[1]) ? $tmp[1] : '');
 
         @endphp
 
